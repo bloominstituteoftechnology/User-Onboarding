bbc515abca32b8787326d5bf43bc9ab79160bc8f
Elise Spain <elisespain@gmail.com>
2019-07-17 12:56:26 -0700

    Setup form inputs

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Form from './Components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Green is my fav color</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/Components/Form.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+
