f6147aa4d57f93a4a938aa8f2f4e23e41c02eae7
dannyvidal <vidaldanny94@gmail.com>
2019-08-07 14:37:22 -0400

    added submit handler

--- user-onboarding/src/components/form.jsx
+++ user-onboarding/src/components/form.jsx
@@ -4,47 +4,53 @@ import "react-toastify/dist/ReactToastify.css";
 import { Formik, Field } from "formik";
 
 /*
- Name
-- Email
-- Password
-- Terms of Service (checkbox)
-- A Submit button to send our form data to the server.
+handle submit 
+validation 
+toastify
 */
 
 class Form extends Component {
   
+    constructor(){
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+handleSubmit(e){
+    e.preventDefault();
+    console.log("submitted");
+}
   render() {
     return (
-      <React.Fragment>
-        <form>
+        <React.Fragment>
         <Formik 
         initialValues={{
             email:"",
             password:"",
             termsOfService:false,
         }}
+
             render={() => {
                return (
-                    <React.Fragment>
-                   <div className="form-group">
-                       <label forHtml="email">Email</label>
-                       <Field className="form-control" type="email" name="email" placeHolder="Email" />
-                   </div>
-                   <div className="form-group">
-                       <label htmlFor="password">Password</label>
-                       <Field className="form-control" type="password" name="password" placeHolder="Password" />
-                   </div>
-                   <div className="form-check">
-                       
-                       <input type="checkbox" className="form-check-input" name="termsOfService" />
-                       <label htmlFor="termsOfService" className="form-check-label">I accept the Terms Of Service</label>
-                   </div>
-                   </React.Fragment>  
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <label forHtml="email">Email</label>
+                            <Field className="form-control" type="email" name="email" placeHolder="Email" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <Field className="form-control" type="password" name="password" placeHolder="Password" />
+                        </div>
+                        <div className="form-check">
+                            
+                            <input type="checkbox" className="form-check-input" name="termsOfService" />
+                            <label htmlFor="termsOfService" className="form-check-label">I accept the Terms Of Service</label>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                 </form>
                )
             }}
         />
-        <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        
        
         <ToastContainer />
       </React.Fragment>
