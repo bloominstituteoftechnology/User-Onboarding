761ed811e430a1074bd80335d9aaa067a70e5574
noahfranco <noahfra@gmail.com>
2019-08-07 16:04:01 -0400

    adding field

---
+++ user-onboarding/src/components/UserForm.js
@@ -0,0 +1,16 @@
+import React from "react"; 
+import axios from 'axios';
+import * as Yup from 'yup';
+import { Form, Field, withFormik } from "formik"; 
+
+const UserForm = () => {
+ return(
+    <div className='user-form'> 
+    <h1>User</h1>
+    <Form>
+     
+     
+    </Form> 
+    </div>
+ )   
+}
