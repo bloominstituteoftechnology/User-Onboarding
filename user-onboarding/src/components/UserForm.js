76d70d09f3cd08945da69f034d468c2a2005e2d6
Greg Johnston <GCJohnston91@gmail.com>
2019-08-07 16:59:43 -0400

    Form and axios working. Setting up data render now

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -1,31 +1,35 @@
 import React from 'react'
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios'
 
-const UserForm = ({ values, handleChange }) => {
+const UserForm = ({ values, handleChange, errors, touched }) => {
 
   return(
     <div className="form-div">
       <h1>New User Onboarding</h1>
-      <div>
         <Form className="form-wrapper">
+          <div>
+            {touched.name && errors.name && <p>{errors.name}</p>}
           <Field type='name' name='name' placeholder='Name' />
+          </div>
+          <div>
+            {touched.email && errors.email && <p>{errors.email}</p>}
           <Field type='email' name='email' placeholder='Email' />
+          </div>
+          <div>
+            {touched.password && errors.password && <p>{errors.password}</p>}
           <Field type='password' name='password' placeholder='Password' />
-          <label>
-            <Field type="checkbox" name="tos" checked={values.tos} />
-            Please agree to Terms of Service
-          </label>
-          <label>
-            How did you hear about us?
-            <Field component="select" name="hearaboutus">
-              <option value="internet">Internet</option>
-              <option value="other">Other</option>
-            </Field>
-          </label>
-          <button>Submit</button>
+          </div>
+          <div>
+            {touched.tos && errors.tos && <p>{errors.tos}</p>}
+            <label>
+              <Field type="checkbox" name="tos" checked={values.tos} />
+              Please agree to Terms of Service
+            </label>
+          </div>
+          <button type="submit">Submit</button>
         </Form>
-      </div>
     </div>
     )
 }
@@ -37,11 +41,21 @@ const FormikUserForm = withFormik({
       email: email || '',
       password: password || '',
       tos: tos || false,
-      hearaboutus: hearaboutus || 'internet'
     }
   },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required('Please enter a name'),
+    email: Yup.string().email('Email not valid').required('Valid email required'),
+    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Please enter a valid password'),
+    tos: Yup.bool().oneOf([true],('You must agree to the terms of service'))
+  }),
   handleSubmit(values) {
     console.log(values);
+    axios
+      .post('https://reqres.in/api/users/', values)
+      .then(res => {
+        console.log(res);
+      })
   }
 })(UserForm)
 
