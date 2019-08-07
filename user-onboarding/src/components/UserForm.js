a904e4057e016b53322e14e7d8477e646255742f
Greg Johnston <GCJohnston91@gmail.com>
2019-08-07 16:39:15 -0400

    Form done. Working on validation now

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -7,11 +7,22 @@ const UserForm = ({ values, handleChange }) => {
   return(
     <div className="form-div">
       <h1>New User Onboarding</h1>
-      <div className="form-wrapper">
-        <Form>
-          <input type='name' name='name' placeholder='Name' value={values.name} onChange={handleChange}/>
-          <input type='email' name='email' placeholder='Email' value={values.email} onChange={handleChange}/>
-          <input type='password' name='password' placeholder='Password' value={values.password} onChange={handleChange}/>
+      <div>
+        <Form className="form-wrapper">
+          <Field type='name' name='name' placeholder='Name' />
+          <Field type='email' name='email' placeholder='Email' />
+          <Field type='password' name='password' placeholder='Password' />
+          <label>
+            <Field type="checkbox" name="tos" checked={values.tos} />
+            Please agree to Terms of Service
+          </label>
+          <label>
+            How did you hear about us?
+            <Field component="select" name="hearaboutus">
+              <option value="internet">Internet</option>
+              <option value="other">Other</option>
+            </Field>
+          </label>
           <button>Submit</button>
         </Form>
       </div>
@@ -20,11 +31,13 @@ const UserForm = ({ values, handleChange }) => {
 }
 
 const FormikUserForm = withFormik({
-  mapPropsToValues({ name, email, password }) {
+  mapPropsToValues({ name, email, password, tos, hearaboutus }) {
     return {
       name: name || '',
       email: email || '',
       password: password || '',
+      tos: tos || false,
+      hearaboutus: hearaboutus || 'internet'
     }
   },
   handleSubmit(values) {
