9499ab0ea4296586f5c13cb1b5bfcc8edb984c88
Chike Chris <cchike2017@gmail.com>
2019-08-07 17:08:20 -0400

    made a post request

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -1,33 +1,69 @@
 import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import axios from "axios";
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 
-const UserForm = ({ errors, touched, values }) => {
+const UserForm = ({ errors, touched, values, handleChange, status }) => {
+  const [users, setUsers] = useState([]);
+  console.log(users);
+
+  useEffect(() => {
+    if (status) {
+      setUsers([...users, status]);
+    }
+  }, [status]);
+
   return (
     <div className="userform">
       <Form>
-        <Field type="text" name="name" placeholder="Name" />
+        <Field
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+        />
         {touched.name && errors.name && <p className="error">{errors.name}</p>}
 
-        <Field type="text" name="email" placeholder="Email" />
+        <Field
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
         {touched.email && errors.email && (
           <p className="error">{errors.email}</p>
         )}
 
-        <Field type="text" name="password" placeholder="Password" />
+        <Field
+          type="text"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
         {touched.password && errors.password && (
           <p className="error">{errors.password}</p>
         )}
 
         <label className="checkbox-container">
           Terms of Service
-          <Field type="checkbox" name="terms" checked={values.terms} />
+          <Field
+            type="checkbox"
+            name="terms"
+            checked={values.terms}
+            onChange={handleChange}
+          />
           <span className="checkmark" />
         </label>
 
         <button type="submit">Submit!</button>
       </Form>
+
+      {users.map(user => (
+        <div className=">user-card">
+          <p key={user.id}>{user.name}</p>
+          <p key={user.id}>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 };
@@ -46,7 +82,16 @@ const FormikForm = withFormik({
     name: Yup.string().required("Please enter your name!"),
     email: Yup.string().required("Pleae enter your email!"),
     password: Yup.string().required("Please enter a password!")
-  })
+  }),
+
+  handleSubmit(values, { setStatus }) {
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        setStatus(res.data);
+      })
+      .catch(err => console.log(err.response));
+  }
 })(UserForm);
 
 export default FormikForm;
