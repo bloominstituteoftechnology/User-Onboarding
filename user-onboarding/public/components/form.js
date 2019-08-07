d1608e2dd2f7efb2dcd88f1e8df2ca886efdd97d
dangolbeeker <scottb305@outlook.com>
2019-08-07 17:01:32 -0400

    setting up imports

---
+++ user-onboarding/public/components/form.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { withFormik, Form, Field} from 'formik';
+
+
+
---
+++ user-onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import form from './components/form.js'
+
+function App() {
+  return (
+    <div className="App">
+      <form />
+    </div>
+  );
+}
+
+export default App;
