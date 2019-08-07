9dbd4268a3bb1e5cd4a9dae326f4ef9ba5f44803
noahfranco <noahfra@gmail.com>
2019-08-07 18:49:28 -0400

    finished and got MVP

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import UserForm from './components/UserForm';
 
+//Brought to you today by Noah Franco and Jessica Morrison
+
 function App() {
+
+  
+
+  useEffect(() => {
+      
+        console.log("Hello World");          
+           
+  });
+
   return (
     <div>
       <UserForm />
--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -1,16 +1,38 @@
-import React from "react"; 
+import React, { useState, useEffect } from "react"; 
 import axios from 'axios';
 import * as Yup from 'yup';
 import { Form, Field, withFormik } from "formik"; 
 
- const UserForm = () => {
+ const UserForm = ({ errors, touched, values, status }) => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        if (status) {  
+            console.log(status);           
+            setUsers([...users, status]);
+        }        
+    }, [status]);
+
+    
  return(
     <div className='user-form'> 
     <h1>User</h1>
     <Form>
     <Field type="text" name="name" placeholder="Name" />
+    {touched.name && errors.name && (
+        <p className="error">{errors.name}</p>
+    )}
+
     <Field type="email" name="email" placeholder="Email" />
+    {touched.email && errors.email && (
+          <p className="error">{errors.email}</p>
+        )}
+
     <Field type="password" name="password" placeholder="Password" />
+    {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
 
     <label className="checkbox-container">
           Accept Terms of Service
@@ -22,11 +44,18 @@ import { Form, Field, withFormik } from "formik";
           <span className="checkmark" />
         </label>
 
-
-
     <button type='submit'>Submit</button>
      
     </Form> 
+        {users.map(user => (
+            <div>
+                <p>
+                { user.name }
+                { user.email }
+                </p>
+            </div>
+        ))}
+
     </div>
  )   
 }
@@ -38,24 +67,36 @@ const FormikUserForm = withFormik({
             name: name || '',
             email: email || '',
             password: password || '',
-            tos: tos || false
+            tos: tos || true
         };
     },
 
     validationSchema: Yup.object().shape({
-    name: Yup.string().required(),
-    email: Yup.string().required(),
-    password: Yup.string().required(),
-    // tos: Yup.   
+    name: Yup.string().required('YOU MESSED UP '),
+    email: Yup.string().required('YOU MESSED UP'),
+    password: Yup.string().required('YOU MESSED UP SON') 
     }),    
 
-handleSubmit(values) {
-    axios
-    .post(" https://reqres.in/api/users", values )
-    .then(res => console.log(res))
-    .catch(error => console.log("ERROR", error))
-}    
 
+
+handleSubmit(values, { resetForm ,setErrors, setStatus }) {
+    if (values.email === 'cats@fish.com') {
+        setErrors({ email: 'That email is taken, SON'})
+    } else {
+        axios
+        .post(" https://reqres.in/api/users", values)
+        .then(res => {
+            console.log(res);
+            setStatus(res.data); 
+            resetForm(); 
+        })    
+        .catch(error => {
+            console.log("ERROR", error);
+        });
+    }   
+}
 })(UserForm);
+
+
 
 export default FormikUserForm;
