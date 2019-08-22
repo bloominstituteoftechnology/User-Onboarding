7816df358a0d5cd9b30599b9473dbdbba5b5155e
zachary-peasley <zpman95@hotmail.com>
2019-08-22 14:50:10 -0500

    Added first field

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,8 +1,32 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import { Form, Field, Formmik } from 'formik';
 import * as Yup from 'yup';
 
-const UserForm  = ({ error, touched, values, handleSubmit, status}) => {
-    
+const UserForm  = ({ errors, touched, values, handleSubmit, status}) => {
+    const [user, setUser] = useState([]);
+    console.log(user);
+
+    useEffect(() => {
+        if (status) {
+            setUser([...user, status]);
+
+        }
+    }, [status]);
+    console.log(status);
+
+return (
+    <div className='container'>
+        <h1>New User</h1>
+        <Form className='field'
+         type="text"
+         name="name"
+        placeholder = "Name"/>
+        {touched.species && errors.species && (
+          <p className="error">{errors.species}</p>
+        )}
+
+    </div>
+)
+
 }
