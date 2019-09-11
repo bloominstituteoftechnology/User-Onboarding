66456b727b646a089a4ff5e2f46d657bbf47b999
spettigrew <sara.pettigrew5@gmail.com>
2019-09-11 17:31:45 -0600

    JS added and checking errors

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -8,13 +8,11 @@ function NewUser({ values, errors, touched}) {
         <Form>
             <Field type="text" name="name" placeholder="Name" />
             <div>
-                {touched.email && errors.email &&
-                <p>{errors.email}</p>} 
+                {touched.email && errors.email && <p>{errors.email}</p>} 
             <Field type="email" name="email" placeholder="Email" />
             </div>
             <div>
-                {touched.password && errors.password && 
-                <p>{errors.password}</p>}
+                {touched.password && errors.password && <p>{errors.password}</p>}
                 <Field type="password" name="password" placeholder="Password" />
             </div>
             <label>
@@ -45,22 +43,25 @@ const FormikNewUser = withFormik({
             .required("Password required")
     }),
  
-    handleSubmit(values) {
-        console.log(values);
-        //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
+    handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+        if (values.email === "") {
+            setErrors({ email: "That email is already taken" });
+        } else {
+            axios
+                .post("https://reqres.in/api/users_", values)
+                .then(response => {
+                    console.log(response); 
+                    resetForm();
+                    setSubmitting(false);
+                })
+                .catch(error => {
+                    console.log(error); 
+                    setSubmitting(false);
+                });
+        }
     }
-});
+})
 
 export default NewUser;
 
-const sentData = {};
-
-axios
-    .post("", sentData)
-    .then(response => {
-        console.log(response.data); 
-    })
-    .catch(error => {
-        console.log(error); 
-    });
     // Always console log the servers response!!
