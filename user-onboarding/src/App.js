8ac972828d2fb6d6cbebd913f42c79f39bfa94ad
spettigrew <sara.pettigrew5@gmail.com>
2019-09-11 16:59:41 -0600

    Notes and Post added to form.js

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import NewUser from './components/Form';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+       <NewUser />
       </header>
     </div>
   );
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-function NewUser() {
+function NewUser({ values, errors, touched}) {
     return (
         <Form>
             <Field type="text" name="name" placeholder="Name" />
+            <div>
+                {touched.email && errors.email &&
+                <p>{errors.email}</p>} 
             <Field type="email" name="email" placeholder="Email" />
-            <Field type="password" name="password" placeholder="Password" />
-            <Field type="checkbox" name="checkbox" placeholder="Terms of Service" />
+            </div>
+            <div>
+                {touched.password && errors.password && 
+                <p>{errors.password}</p>}
+                <Field type="password" name="password" placeholder="Password" />
+            </div>
+            <label>
+            <Field type="checkbox" name="check" checked={values.check} placeholder="Terms of Service" />
+            </label>
            
             <button>Submit!</button>
         </Form>
@@ -15,15 +27,24 @@ function NewUser() {
 }
 
 const FormikNewUser = withFormik({
-    mapPropsToValues({ email, password }) {
+    mapPropsToValues({ name, email, password, checkbox }) {
         return {
             name: name || "",
             email: email || "",
-            password: password || ""
-            checkbox: checkbox || "",
+            password: password || "",
+            checkbox: checkbox || ""
         };
     },
 
+      validationSchema: Yup.object().shape({
+        email: Yup.string()
+            .email("Email not valid")
+            .required("Email required"),
+        password: Yup.string()
+            .min(8, "Password must be 8 or more characters")
+            .required("Password required")
+    }),
+ 
     handleSubmit(values) {
         console.log(values);
         //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
@@ -31,3 +52,15 @@ const FormikNewUser = withFormik({
 });
 
 export default NewUser;
+
+const sentData = {};
+
+axios
+    .post("", sentData)
+    .then(response => {
+        console.log(response.data); 
+    })
+    .catch(error => {
+        console.log(error); 
+    });
+    // Always console log the servers response!!
