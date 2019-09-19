500b5245e8182cc5bd7d417fac828c81a7067f2e
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-09-19 17:57:02 -0400

    WIP X 2

--- team-builder/src/components/UserForm.js
+++ team-builder/src/components/UserForm.js
@@ -2,54 +2,63 @@ import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
-import { __values } from "tslib";
 
 const UserForm = ({ values, errors, touched, status }) => {
     const [user, setUser] = useState([]);
     useEffect(() => {
-        if (status) {
-            setUser([...user, status]);
-        }
+        status && setUser(user => [...user, status]);
     }, [status]);
+
+    return (
+        <div className="user-form">
+            <Form>
+                <Field type="text" name="name" placeholder="name" />
+                {touched.name && errors.name && (
+                    <p className="error">{errors.name}</p>
+                )}
+
+                <Field type="text" name="email" placeholder="Email" />
+                {touched.email && errors.email && <p className="errors">{errors.email}</p>}
+
+                <label>
+                    Terms of Service
+                <Field>
+                        type="checkbox"
+                        name="Terms of Service"
+                    checked={values.termsOfService}
+                    </Field>
+                </label>
+                <button>Submit!</button>
+            </Form>
+            {user.map(user => (
+                <ul key={user.id}>
+                    <li>Name:{user.name}</li>
+                    <li>Email:{user.email}</li>
+                </ul>
+            ))}
+        </div>
+    )
 };
 
-return (
-    <div className="user-form">
-        <Form>
-            <Field type="text" name="name" placeholder="name" />
-            {touched.name && errors.name && (
-                <p className="error">{errors.name}</p>
-            )}
-
-            <Field type="text" name="email" placeholder="Email" />
-            {touched.email && errors.email && <p className="errors">{erros.email}</p>}
-
-            <label>
-                Terms of Service
-                <Field>
-                    type="checkbox"
-                    name="Terms of Service"
-                    checked={values.termsOfSercice}
-                </Field>
-            </label>
-            <button>Submit!</button>
-        </Form>
-        {animals.map(animal => (
-            <ul key={user.id}>
-                <li>Name:{user.name}</li>
-                <li>Email:{user.email}</li>
-            </ul>
-        ))}
-
-    </div>
-)
 
 const FormikUserForm = withFormik({
     mapPropsToValues({ name, email, password }) {
         return {
+            name: name || "",
+            email: email || "",
 
-        }
+        };
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required,
+        email: Yup.string().required()
+    }),
+
+    handleSubmit(values, { setStatus }) {
+        axios
+            .post("https://reqres.in/api/users", values)
     }
-})
+})(UserForm);
 
 export default FormikUserForm;
