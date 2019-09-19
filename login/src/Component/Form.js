5c055a6f16e8a1546b5041a7d2eb55a43f23c840
Manju KC <kcmanjuthapa@gmail.com>
2019-09-19 16:23:35 -0400

    added checkbox

--- login/src/Component/Form.js
+++ login/src/Component/Form.js
@@ -1,35 +1,38 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
 
-function OnBoardForm({values}) {
+function OnBoardForm({ values }) {
   return (
     <Form>
-      <input id="name" type="text" name="name" placeholder="name" />
-      <input id="email" type="Email" name="email" placeholder="email" />
-      <input
-        id="password"
+      <Field  type="text" name="name" placeholder="name" />
+      <Field  type="Email" name="email" placeholder="email" />
+      <Field
         type="password"
         name="password"
         placeholder="password"
       />
-      <button>Submit</button>
+       <Field
+            type="checkbox"
+            name="checkbox"
+            checked={values.checkbox}
+          />
+      <button>Submit!</button>
     </Form>
   );
 }
 
 const FormikOnBoardForm = withFormik({
-  mapPropsToValues(name, email, password) {
+  mapPropsToValues(name, email, password, checkbox) {
     return {
-        name:name || "",
-        email:email || "",
-        password:password || ""
+      name: name || "",
+      email: email || "",
+      password: password || "",
+      checkbox:checkbox || ""
     };
   },
   handleSubmit(values) {
-console.log(values)
+    console.log(values);
   }
 })(OnBoardForm);
 
-
-
-export default FormikOnBoardForm ;
+export default FormikOnBoardForm;
