6feca0f7b9c781b8b768d0c081322adf5fb5e515
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-19 14:56:35 +0100

    created a form tag and field tag with a placeholder

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Form from './Form'; 
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {/* <Form/> */}
+
     </div>
   );
 }
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Formik, Field } from 'formik';
+import axios from 'axios';
+
+function Form() {
+
+    return (
+        <Form>
+            <label>
+                <Field name='' type='' placeholder='' />
+            </label>
+
+            <label>
+                <Field name='' type='' placeholder='' />
+            </label>
+
+            <label>
+                <Field name='' type='' placeholder='' />
+            </label>
+
+            <label>
+                <Field name='' type='' placeholder='' />
+            </label>
+
+            <label>
+                <Field name='' type='' placeholder='' />
+            </label>
+
+        </Form>
+
+    )
+
+}
+
+export default Form
