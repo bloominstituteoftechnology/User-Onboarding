d289a3b676b16ec3a09d669c3021f526a8cb94a6
Manju KC <kcmanjuthapa@gmail.com>
2019-09-19 16:36:20 -0400

    added validationSchema underneath mapPropsT Values and added error message in first input

--- login/src/Component/Form.js
+++ login/src/Component/Form.js
@@ -2,10 +2,14 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-function OnBoardForm({ values }) {
+function OnBoardForm({ values, errors, touched }) {
+
   return (
     <Form>
       <Field  type="text" name="name" placeholder="name" />
+      {touched.name && errors.name && (
+          <p className="error">{errors.name}</p>
+        )}
       <Field  type="Email" name="email" placeholder="email" />
       <Field
         type="password"
@@ -31,6 +35,12 @@ const FormikOnBoardForm = withFormik({
       checkbox:checkbox || ""
     };
   },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("You must put a name"),
+   
+  }),
+
+
   handleSubmit(values) {
     console.log(values);
   }
