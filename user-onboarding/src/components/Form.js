0f05684f04085b7b2985fc6d5ea91b49a57472b9
admin <admin@admins-MacBook-Pro.local>
2019-09-19 12:50:32 -0700

    added form and field foundation

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -5,13 +5,32 @@ import { withFormik, Form, Field } from 'formik';
 function Forms (){
 
      return(
-          <div>Hello I am form</div>
+          <div>
+               <Form>
+                    <Field type="text" name="name" placeholder="Name" />
+                    <Field type="email" name="email" placeholder="Email" />
+                    <Field type="password" name="password" placeholder="Password" />
+
+                    <label> Terms of Service: 
+                    <Field type="checkbox" name="termsOfService" />
+                    </label>
+               </Form>
+
+               <button>Submit</button>
+          </div>
      )
 }
 
 const FormikForms = withFormik({
-     
+     mapPropsToValues({name, email, password, termsOfService}){
+          return{
+               name: name || '',
+               email: email || '',
+               password: password || '',
+               termsOfService: termsOfService || false
+          }
+     }
 })(Forms)
 console.log(FormikForms)
 
-export default Forms
+export default FormikForms
