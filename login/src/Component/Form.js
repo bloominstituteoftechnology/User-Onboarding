3e533b1665e14d549ab6785f42fea655153ec36f
Manju KC <kcmanjuthapa@gmail.com>
2019-09-19 17:08:18 -0400

    imported useState and useEffect and added useEffect function

--- login/src/Component/Form.js
+++ login/src/Component/Form.js
@@ -1,32 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-function OnBoardForm({ values, errors, touched }) {
-    const [users, setusers] = useState([]);
+function OnBoardForm({ values, errors, touched, status }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (status) {
+      setUsers([...users, status]);
+    }
+  }, [status]);
+
   return (
     <Form>
-      <Field  type="text" name="name" placeholder="name" />
-      {touched.name && errors.name && (
-          <p className="error">{errors.name}</p>
-        )}
-      <Field  type="Email" name="email" placeholder="email" />
-      {touched.email && errors.email && (
-          <p className="error">{errors.email}</p>
-        )}
-      <Field
-        type="password"
-        name="password"
-        placeholder="password"
-      />
-       {touched.password && errors.password && (
-          <p className="error">{errors.password}</p>
-        )}
-       <Field
-            type="checkbox"
-            name="checkbox"
-            checked={values.checkbox}
-          />
+      <Field type="text" name="name" placeholder="name" />
+      {touched.name && errors.name && <p className="error">{errors.name}</p>}
+      <Field type="Email" name="email" placeholder="email" />
+      {touched.email && errors.email && <p className="error">{errors.email}</p>}
+      <Field type="password" name="password" placeholder="password" />
+      {touched.password && errors.password && (
+        <p className="error">{errors.password}</p>
+      )}
+      <Field type="checkbox" name="checkbox" checked={values.checkbox} />
       <button>Submit!</button>
     </Form>
   );
@@ -45,9 +40,7 @@ const FormikOnBoardForm = withFormik({
     name: Yup.string().required("You must put a name"),
     email: Yup.string().required("You must put a email"),
     password: Yup.string().required("You must enter password")
-   
   }),
-
 
   handleSubmit(values) {
     console.log(values);
