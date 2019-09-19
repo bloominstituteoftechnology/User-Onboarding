13556599b060321ab5552fc42e087cee4ab2bf62
Manju KC <kcmanjuthapa@gmail.com>
2019-09-19 15:43:04 -0400

    added mapPropsToValues

--- login/src/Component/Form.js
+++ login/src/Component/Form.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { withFormik, Form, Field} from "formik";
+import { withFormik, Form, Field } from "formik";
 
 function OnBoardForm() {
   return (
     <form>
       <input id="name" type="text" name="name" placeholder="name" />
       <input id="email" type="Email" name="email" placeholder="email" />
-      <input id="password" type="password" name="password" placeholder="password" />
-    <button>Submit</button>
+      <input
+        id="password"
+        type="password"
+        name="password"
+        placeholder="password"
+      />
+      <button>Submit</button>
     </form>
   );
 }
 
-export default OnBoardForm;
+const FormikOnBoardForm = withFormik({
+  mapPropsToValues() {
+    return {};
+  }
+})(OnBoardForm);
+
+export default FormikOnBoardForm ;
