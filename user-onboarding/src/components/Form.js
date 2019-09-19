5c503c5cb0328b89e74fe5ade406f5a6d70b94ed
meera-andersen <meera.andersen@gmail.com>
2019-09-19 16:32:44 -0600

    added dropdown with error message as well as error message for unchecked checkbox

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -29,17 +29,32 @@ const UserForm = ({ values, errors, touched, status }) => {
                     <p className="error">{errors.password}</p>
                 )}
                 <Field type="password" name="password" placeholder="Create a password"/>
+                
+                {touched.role && errors.role && (
+                    <p className="error">{errors.role}</p>
+                )}
+                <Field component="select" name="role">
+                    <option value="" disabled>Select a Role</option>
+                    <option value="nerd">Nerd</option>
+                    <option value="dingus">Dingus</option>
+                    <option value="wizard">Wizard</option>
+                </Field>
+
+                {touched.terms && errors.terms && (
+                    <p className="error">{errors.terms}</p>
+                )}
                 <label>
                     Agree to our Terms of Service
-                    <Field type="checkbox" name="terms" checked="values.terms"  />
+                    <Field type="checkbox" name="terms" checked={values.terms}  />
                 </label>
-                <button>Create Account</button>
+                <button type="submit">Create Account</button>
             </Form>
             {users.map(user => (
                 <div key={user.id}>
                     <h1>Name: {user.name}</h1>
                     <p>Email: {user.email}</p>
                     <p>Password: {user.password}</p>
+                    <p>Role: {user.role}</p>
                 </div>
             ))}
         </div>
@@ -52,6 +67,7 @@ const FormikUserForm = withFormik ({
             name: values.name || "",
             email: values.age || "",
             password: values.password || "",
+            role: values.role || "",
             terms: values.terms || false,
         }
     },
@@ -59,7 +75,9 @@ const FormikUserForm = withFormik ({
     validationSchema: Yup.object().shape({
         name: Yup.string().required("Please enter your name"),
         email: Yup.string().required("Please enter a valid email"),
-        password: Yup.string().min(9).required("please enter a password with at least 9 characters")
+        password: Yup.string().min(9).required("please enter a password with at least 9 characters"),
+        role: Yup.string().required("Please select a ole"),
+        terms: Yup.bool().oneOf([true], "You must agree to our Terms and Services to continue")
     }),
 
     handleSubmit(values, { setStatus }) {
@@ -73,5 +91,7 @@ const FormikUserForm = withFormik ({
 
 
 })(UserForm);
+
+console.log("This is the User Data", FormikUserForm);
 
 export default FormikUserForm;
