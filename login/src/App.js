9aee2ace63a195b4d18bebdb574999e2793cc827
Manju KC <kcmanjuthapa@gmail.com>
2019-09-19 15:22:55 -0400

    created the Form and imported in app.js

--- login/src/App.js
+++ login/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Form from './Component/Form'
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
       <h1>Hello</h1>
+      <Form/>
       </div>
     );
   }
--- login/src/Component/Form.js
+++ login/src/Component/Form.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 function Form() {
-
-
+  return (
+    <form>
+      <input id="name" type="text" name="name" placeholder="name" />
+      <input id="email" type="Email" name="email" placeholder="email" />
+      <input id="password" type="password" name="password" placeholder="password" />
+    <button>Submit</button>
+    </form>
+  );
 }
 
 export default Form;
