4bf96d0f9ca5ee1530eda794bb917b72d642f0fc
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-17 14:44:37 -0700

    Add Validation for name and password with Error and touch on field items.

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,24 +1,26 @@
 import React, {useState} from 'react';
 // import axios from "axios";
 import { withFormik, Form, Field } from "formik";
-// import * as Yup from "yup";
+import * as Yup from "yup";
 
-function UserForm({values}) {
+function UserForm({values, touched, errors}) {
     // const [user, setUser] = useState([])
     return (
         <div>
             <Form>
                 <Field type="text" name="name" placeholder="name"/>
+                {touched.name && errors.name && <p className="error">{errors.name}</p>}
                 <Field type="text" name="email" placeholder="email"/>
                 <Field type="text" name="password" placeholder="password"/>
+                {touched.password && errors.password && <p className="error">{errors.password}</p>}
                 <label>
                     Terms of Service
                     <Field type="checkbox" 
                     name="termsOfService" 
                     checked={values.termsOfService} 
                     />
-                    
                 </label>
+                <button type="submit">Submit!</button>
             </Form>
         </div>
     )
@@ -32,6 +34,10 @@ const FormikUserForm = withFormik ({
             password: password || "",
             termsOfService: termsOfService || false
         };
-    }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("Please enter name"),
+        password: Yup.string().required("No Open Sesame, No pass Go, Please Enter your password!"),
+    }),
 })(UserForm);
 export default FormikUserForm;
