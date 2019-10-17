9c0aa0233bd6433925bff6a831b375a65bc6bfd8
Brendan Schatz <brendanschatz97@gmail.com>
2019-10-17 18:13:17 -0400

    MVP

---
+++ onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NewUser from "./form"
+
+function App() {
+  return (
+    <div className="App">
+      <NewUser />
+    </div>
+  );
+}
+
+export default App;
