318952cae3e6d102f54ee977bad09e60b4b2a0c9
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-17 13:30:40 -0700

    Build Form. Connect Form to withFormik function.

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -3,23 +3,33 @@ import React, {useState} from 'react';
 import { withFormik, Form, Field } from "formik";
 // import * as Yup from "yup";
 
-function UserForm() {
+function UserForm({values}) {
     // const [user, setUser] = useState([])
     return (
         <div>
             <Form>
                 <Field type="text" name="name" placeholder="name"/>
+                <Field type="text" name="email" placeholder="email"/>
+                <Field type="text" name="password" placeholder="password"/>
+                <label>
+                    
+                    Terms of Service
+                    <Field type="checkbox" name="termsOfService" checked="{values.termsOfService}" />
 
+                </label>
             </Form>
         </div>
     )
 }
 
-const FormikUserForm = withFormik({
-    mapPropsToValues({name}){
+const FormikUserForm = withFormik ({
+    mapPropsToValues({name, email, password, termsOfService }){
         return{
             name: name || "",
-        }
+            email: email || "",
+            password: password || "",
+            termsOfService: termsOfService || false,
+        };
     }
 })(UserForm);
 export default FormikUserForm;
