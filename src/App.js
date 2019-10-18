40e14018542964bfd84f7095fa6e7f3ba9467c8f
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-10-17 22:20:56 -0500

    styles added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import UserForm from "./components/UserForm";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+
+  
+
+<div className="App">
+  
+
+
       <UserForm />
+    
     </div>
   );
 }
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -7,36 +7,39 @@ const UserForm = ({errors, touched, values, status}) => {
 
     const [userName, setUserName] = useState([]);
 
+    
+
     useEffect (() =>{
         status && setUserName (userName => [...userName, status]);
     },[status]);
 
 
     return (
-    <div>
+    <div className="main">
+
         <h1> User Onboarding </h1>
         
         <Form>
-        <p>Username </p>
-            <Field type="text"name="user" placeholder="Username"/>
+        
+            <Field className="user-field" ype="text"name="user" placeholder="Username"/>
             {touched.user && errors.user && (
             <p className="error">{errors.user}</p>
             )}
             
         
-        <p>Email </p>
-            <Field type="text"name="email" placeholder="Enter Email"/>
+    
+            <Field className="user-field" type="text"name="email" placeholder="Email"/>
             {touched.email && errors.email && (
             <p className="error">{errors.email}</p>
             )}
 
-        <p>Password </p>
-            <Field type="password"name="password" placeholder="Enter Password"/>
+      
+            <Field className="user-field" type="password"name="password" placeholder="Password"/>
             {touched.password && errors.password && (
             <p className="error">{errors.password}</p>
             )}
 
-            <Field className="dropdown" component="select" name="options">
+            <Field className="user-field" component="select" name="options" >
                 <option>Please Choose an Option</option>
                 <option value="UI-Developer">UI Developer</option>
                 <option value="Front-End">Front End Developer</option>
@@ -45,41 +48,45 @@ const UserForm = ({errors, touched, values, status}) => {
             {touched.options && errors.options && <p className="error">{errors.options}</p>}
 
         
-            <label>
-                {" "}
-                <p>Terms and Conditions </p>
+            <label className="checkbox-container">
+         
+                Terms and Conditions
             <Field
+            className="user-field"
             type="checkbox"
             name="terms"
             checked={values.terms}
             />
-            <span/>
+            <span className="checkmark"/>
+           
             </label> 
             {touched.terms && errors.terms && (
-            <p className="error">{errors.terms}</p>
+            <p className="checkbox-container">{errors.terms}</p>
             )}
 
         
             
         <button type="submit">Submit</button>
 
+
         </Form>
 
-     
+<label className="box-container">
 
     {userName.map(memberId =>(
-        <div key= {userName.Id}>
+        <div className="box" key= {userName.Id}>
             <p> Username: {memberId.user}</p>
             <p> Email: {memberId.email}</p> 
+            <p> Developer: {memberId.options}</p> 
         </div>
-
     ))}
     
-        
+    </label>
     </div>
+  
+
 );
 }
-
 
 
 const FormikUserForm = withFormik({
@@ -100,12 +107,16 @@ const FormikUserForm = withFormik({
       user: Yup.string().required(),
       email: Yup.string().required(),
       password: Yup.string(),
-      terms: Yup.boolean().required().oneOf([true], "Must agree to terms and conditions"), 
-     options: Yup.string().notOneOf(["Please Choose an Option"]).required("Please select one")
+      terms: Yup.boolean().required().oneOf([true], "Checkbox Required"), 
+     options: Yup.string().notOneOf(["Please Choose an Option"]).required()
     }),
 
   
-  
+
+
+
+
+    
     handleSubmit(values, { setStatus, resetForm }) {
       axios
         .post("https://reqres.in/api/users/", values)
