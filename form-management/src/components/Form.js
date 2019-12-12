e34e8a90af01cffd581131123f8bd91cd0d7603b
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2019-12-11 22:39:30 -0800

    added style

--- form-management/src/components/Form.js
+++ form-management/src/components/Form.js
@@ -3,91 +3,92 @@ import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-function Form1({ values, errors, touched, isSubmitting, status }) {
+function Form1({ values, errors, touched,  status }) {
+console.log("values", values);
+  console.log("errors", errors);
+  console.log("touched", touched);
+
     const [users, setUsers] = useState([]);
 
-        useEffect(() => {
-          if (status) {
-            setUsers([...users, status]);
-          }
-        }, [status]);
+    useEffect(() => {
+       status && setUsers(users => [...users, status]); 
+    }, [status]);
 
     return (
-        <div>
+        <div className="user-form">
             <Form >
                 <div>
-                    {touched.username && errors.username && <p>{errors.username}</p>}
-                    <Field type="text" name="username" placeholder="Username" />
+                   <Field type="text" name="username" placeholder="Username" />
+                   {touched.username && errors.username && <p className="errors"> {errors.username}</p>}
                 </div>
                 <br />
                 <div>
-                    {touched.password && errors.password && <p>{errors.password}</p>}
                     <Field type="password" name="password" placeholder="Password" />
+                    {touched.password && errors.password && <p className="errors">{errors.password}</p>}
                 </div>
                 <br />
                 <div>
-                    {touched.email && errors.email && <p>{errors.email}</p>}
                     <Field type="email" name="email" placeholder="Email" />
+                    {touched.email && errors.email && <p className="errors">{errors.email}</p>}
                 </div>
                 <br />
 
                 <label>
                     <Field type="checkbox" name="tos" checked={values.tos} />
                     Accept Terms
-            </label>
+                </label>
                 <br />
-                <button type= "submit" >SignUp</button>
+                <button type="submit"> SignUp </button>
             </Form>
-            
-            {users.map(user => (
-                <div key={user.id}>
-                    <h4>User Name: {user.username}</h4>
-                </div>
-            )
-            )}
+
+
+            {users.map(user =>  { 
+                return(
+                    <div key={user.id}>
+                        <h4>User Name: {user.username}</h4>
+                        <h4>Email: {user.email}</h4>
+                    </div>
+                )
+           })}
         </div>
     );
 };
 const FormikForm = withFormik({
-    mapPropsToValues({ username, password, email, tos }) {
+    mapPropsToValues(props) {
         return {
-            username: username || "",
-            password: password || "",
-            email: email || "",
-            tos: tos || false,
+            username: props.username || "",
+            password: props.password || "",
+            email: props.email || "",
+            tos: props.tos || false,
         };
     },
     //======VALIDATION SCHEMA==========
     validationSchema: Yup.object().shape({
         username: Yup.string()
-            .min(4, "user name must be 4 characters minimum")
+            .min(4, "Username must be 4 characters minimum")
             .required("User Name is required"),
         email: Yup.string()
             .email("Email not valid")
             .required("Email is required"),
         password: Yup.string()
-            .min(6, "password must be 6 characters minimum")
+            .min(6, "Password must be 6 characters minimum")
             .required("Password is required")
     }),
     //======END VALIDATION SCHEMA==========
 
-    handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
-        if (values.email === "alreadytaken@atb.dev") {
-            setErrors({ email: "That email is already taken" });
-        } else {
+    handleSubmit(values, { resetForm, setErrors, setSubmitting ,setStatus}) {
             axios
                 .post("https://reqres.in/api/users", values)
                 .then(res => {
-                    console.log("This is the Response", res); // Data was created successfully and logs to console
+                    console.log("This is the Response", res); 
                     resetForm();
-                    setSubmitting(false);
+                    setStatus(res.data);
                 })
                 .catch(err => {
-                    console.log(err); // There was an error creating the data and logs to console
-                    setSubmitting(false);
+                    console.log(err); 
                 });
+            }
         }
-    }
-})(Form1);
+    )(Form1);
 
 export default FormikForm;
