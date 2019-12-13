68a5314b117d95ef44766b6d32d012f9f606e5e5
Kyle Pace <kpace2296@gmail.com>
2019-12-12 22:19:25 -0600

    finished stretch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import UserForm from './Form';
+import styled from 'styled-components';
 
 function App() {
   return (
+
     <div className="App">
-      <h1>User Form</h1>
+      <h1 class='welcome'>Sign in</h1>
       <UserForm />
     </div>
+
   );
 }
 
--- src/Form.js
+++ src/Form.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from "formik";
 import axios from 'axios';
 import * as Yup from 'yup';
+import styled from "styled-components";
+
+const WrapperDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+`
 
 function UserForm({ values, errors, touched, status}) {
 
@@ -14,55 +20,62 @@ function UserForm({ values, errors, touched, status}) {
     return (
         <div>
             <Form>
-                <label htmlFor='name'>
-                    Name
-                    <Field 
-                        id='name' 
-                        type='text' 
-                        name='name' 
-                        placeholder='Enter name'
-                    />
-                </label>
-                <label htmlFor='email'>
-                    Email 
-                    <Field 
-                        id='email' 
-                        type='email' 
-                        name='email' 
-                        placeholder='Enter email'
-                    />
-                    {touched.email && errors.email && (
-                        <p>{errors.email}</p>
-                    )}
-                </label>
-                <label htmlFor='password'>
-                    Password
-                    <Field 
-                        id='password' 
-                        type='password' 
-                        name='password' 
-                        placeholder='Enter password'
-                    />
-                    {touched.password && errors.password && (
-                        <p>{errors.password}</p>
-                    )}
-                </label>
-                <label htmlFor='terms'>
-                    Terms of Service
-                    <Field 
-                        id='terms' 
-                        type='checkbox' 
-                        name='terms' 
-                        checked={values.terms}
-                    />
-                </label>
-                <button type='submit'>Submit</button>
+                <WrapperDiv>
+                    <label class='label' htmlFor='name'>
+                        Name
+                        <Field 
+                            id='name' 
+                            type='text' 
+                            name='name' 
+                            placeholder='Enter name'
+                            className="field"
+                        />
+                    </label>
+                    <label class='label' htmlFor='email'>
+                        Email 
+                        <Field 
+                            id='email' 
+                            type='email' 
+                            name='email' 
+                            placeholder='Enter email'
+                            className="field"
+                        />
+                        {touched.email && errors.email && (
+                            <p>{errors.email}</p>
+                        )}
+                    </label>
+                    <label class='label' htmlFor='password'>
+                        Password
+                        <Field 
+                            id='password' 
+                            type='password' 
+                            name='password' 
+                            placeholder='Enter password'
+                            className="field"
+                        />
+                        {touched.password && errors.password && (
+                            <p>{errors.password}</p>
+                        )}
+                    </label>
+                    <label class='label' htmlFor='terms'>
+                        Terms of Service
+                        <Field 
+                            id='terms' 
+                            type='checkbox' 
+                            name='terms' 
+                            checked={values.terms}
+                            className="field"
+                        />
+                    </label>
+                    <button className='submit' type='submit'>Submit</button>
+                </WrapperDiv>
             </Form>
             {users.map(newUser => {
                 return (
                     <div>
-                        <h3>Name: {newUser.name}</h3>
-                        <h3>Email: {newUser.email}</h3>
+                        <h3 class='label'>Name: {newUser.name}</h3>
+                        <h3 class='label'>Email: {newUser.email}</h3>
+                        <hr></hr>
                     </div>
                 );
             })}
