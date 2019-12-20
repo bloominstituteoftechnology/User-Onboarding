9dceaa0c1337416f68362f1429072334373e6d36
Jonathan <56617776+qb1968@users.noreply.github.com>
2019-12-19 19:35:47 -0500

    MVP

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import MyForm from "./Form"
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyForm/>
+    </div>
+  );
+}
+
+export default App;
