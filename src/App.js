88a505278321a47413f59c301a7438e28af9f60c
mastaryan <sizemore.ryan@gmail.com>
2020-01-31 14:23:47 -0500

    Re-arrange files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import UserForm from "./components/UserForm";
+
+import './App.css';
+
+function App() {
+  return (
+
+  
+
+<div className="App">
+  
+
+
+      <UserForm />
+    
+    </div>
+  );
+}
+
+export default App;
