23b85e98ee12a61aa5783e3e5bdf2a7e9d5c944e
emma <sonicjane2020@gmail.com>
2020-02-27 15:58:49 -0800

    added a form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Form from './components/Form';
+import NewForm from './components/Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <NewForm />
     </div>
   );
 }
--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
-function Form() {
+const NewForm = ({values, touched, errors, status}) => {
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        status && setUser(status);
+    }, [status]);
+
     return (
-      <div className="form">
-        <h1>hello</h1>
+      <div className="container">
+            <Form>
+                <Field type='text' name='name' placeholder='Name:' />
+            </Form>
       </div>
     );
-  }
-  
-  export default Form;
+}  
+  export default NewForm;
