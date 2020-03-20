8079a531186e49f9b4eb7e27200f9d23ec7ac77d
Xaver <xaverpinero@yahoo.com>
2020-03-19 21:29:51 -0400

    added dependencies, made form file

---
+++ myproject/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from "./components/Form";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ myproject/src/components/Form.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Form = props => {
+  return (
+    <button type="submit">Create Member!</button>
+  );
+};
+
+export default Form;
