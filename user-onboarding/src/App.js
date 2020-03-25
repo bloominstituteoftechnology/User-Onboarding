d6fcf01bf5a8e9fc1bd72eb39ab811fc6ee6c678
GraceeElton <60789216+GraceeElton@users.noreply.github.com>
2020-03-25 16:59:15 -0600

    Almost

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Form from "../src/Components/Forms";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>On-boarding sign-up </h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/Components/Forms.js
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+import axios from "axios";
+
+const formSchema = yup.object().shape({
+  Name: yup.string().required("Name is a required field."),
+  Email: yup
+    .string()
+    .email("Must be a valid email.")
+    .required(),
+  Password: yup.string().required("Please enter your password"),
+  Terms: yup.boolean().oneOf([true], "Please agree to terms of use")
+});
+
+export default function Form() {
+  const [button, setButton] = useState(true);
+
+  const [newForm, setForm] = useState({
+    Name: "",
+    Email: "",
+    Password: "",
+    Terms: ""
+  });
+
+  const [error, setError] = useState({
+    Name: "",
+    Email: "",
+    Password: "",
+    Terms: ""
+  });
+
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    formSchema.isValid(newForm).then(valid => {
+      setButton(!valid);
+    });
+  }, [newForm]);
+
+  const formSubmit = event => {
+    event.preventDefault();
+    axios
+      .post("https://reqres.in/api/users", newForm)
+      .then(response => {
+        setPost(response.data);
+        setForm({
+          Name: "",
+          Email: "",
+          Password: "",
+          Terms: ""
+        });
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  const validateChange = event => {
+    yup
+      .reach(formSchema, event.target.Name)
+      .validate(event.target.value)
+      .then(valid => {
+        setError({
+          ...error,
+          [event.target.Name]: ""
+        });
+      })
+      .catch(err => {
+        setError({
+          ...error,
+          [event.target.Name]: err.error
+        });
+      });
+  };
+
+  const inputChange = event => {
+    event.persist();
+    const newFormValue = {
+      ...newForm,
+      [event.target.Name]:
+        event.target.type === "checkbox"
+          ? event.target.checked
+          : event.target.value
+    };
+    validateChange(event);
+    setForm(newFormValue);
+  };
+
+  const submitForm = event => {
+    event.preventDefault();
+    console.log("Form Submitted!");
+  };
+
+  return (
+    <form onSubmit={formSubmit}>
+      <label htmlFor="Name">
+        Name:
+        <input
+          id="Name"
+          type="text"
+          name="Name"
+          value={newForm.Name}
+          onChange={inputChange}
+        />
+        {error.Name.length > 0 ? <p>{error.Name}</p> : null}
+      </label>
+      <br />
+      <label htmlFor="Email">
+        Email:
+        <input
+          id="Email"
+          type="text"
+          name="Email"
+          value={newForm.Email}
+          onChange={inputChange}
+        />
+        {error.Email.length > 0 ? <p>{error.Email}</p> : null}
+      </label>
+      <br />
+      <label htmlFor="Password">
+        Password:
+        <input
+          id="Password"
+          type="password"
+          name="Password"
+          value={newForm.Password}
+          onChange={inputChange}
+        />
+        {error.Password.length > 0 ? <p>{error.Password}</p> : null}
+      </label>
+      <br />
+      <label htmlFor="Terms">
+        <input
+          type="checkbox"
+          name="Terms"
+          checked={newForm.Terms}
+          onChange={inputChange}
+        />
+        Terms and Conditions
+      </label>
+      <br />
+      <pre>{JSON.stringify(post, null, 2)}</pre>
+      <button disabled={button}>Submit</button>
+    </form>
+  );
+}
