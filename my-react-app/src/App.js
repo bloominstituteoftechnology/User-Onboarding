3a1f064030852a1333b3253219f326621927dabd
Wayne <wabbruscatoorr@gmail.com>
2020-03-25 18:22:45 -0400

    updating App file and Form file

---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Forms from "./Forms";
+
+function App() {
+  return (
+    <div className="App">
+      <Forms />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Forms.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Forms = () => {
+  return (
+    <div>
+      <label>
+        Name:
+        <input></input>
+      </label>
+      <label>
+        Email:
+        <input></input>
+      </label>
+      <label>
+        Password:
+        <input></input>
+      </label>
+      <label>
+        Check Box:
+        <input></input>
+      </label>
+    </div>
+  );
+};
+
+export default Forms;
