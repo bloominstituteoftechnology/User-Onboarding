4b6e6967e7fad3a82c4ee07fd8b0e940a2977e3e
Pinefoot <jameslcarpino@gmail.com>
2020-03-25 13:04:44 -0700

    Initialized project, everything installed. Getting to work.

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as yup from 'yup';
+import axios from 'axios';
+import Form from './components/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
