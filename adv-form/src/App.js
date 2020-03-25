425fc38967ffdabf85336b19d97da5d912084adb
Christina Harris <christinathedesigner@outlook.com>
2020-03-25 12:59:36 -0700

    Finished setting up form visuals, working on functionality

--- adv-form/src/App.js
+++ adv-form/src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from "./Form";
+import Styled from "styled-components"
+
+const SuperContainer = Styled.div`
+display: flex;
+flex-wrap: wrap;
+width: 80%;
+margin: 5%;
+border: 1px solid black;
+padding: 5%
+`
+const SuperHeader = Styled.h1`
+display: flex;
+flex-wrap: wrap;
+text-align:center;
+color:white;
+width:100%;
+
+`
+const HeaderDiv = Styled.div`
+background-color: blue;
+width:100%;
+margin-bottom: 3%;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SuperContainer>
+      <HeaderDiv>
+      <SuperHeader>Sign Up Form</SuperHeader>
+      </HeaderDiv>
+      <Form />
+    </SuperContainer>
   );
 }
 
---
+++ adv-form/src/Form.js
@@ -0,0 +1,107 @@
+import React, { useState }from "react";
+import Styled from "styled-components";
+import * as yup from "yup";
+
+
+const superContainer = Styled.div`
+display: flex;
+flex-direction:column;
+`
+const inputDiv = Styled.div`
+margin:15%;
+`
+
+const Form = () => {
+
+
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        password: "",
+        terms:"",
+      });
+
+    const [errors, setErrors] = useState({
+        name: "",
+        email: "",
+        password: "",
+        terms:"",
+    });
+
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+
+    const [post, setPost] = useState([]);
+
+
+      return (
+        <superContainer>
+            <form>
+
+                <inputDiv>
+                    <label htmlFor="name">
+                    Name: 
+                    <input 
+                    id="name" 
+                    type="text" 
+                    name="name"
+                    //value=""
+                    //onChange= 
+                    placeholder="Please Enter your Name" />
+                    </label>
+                </inputDiv>
+
+                <inputDiv>
+                    <label htmlFor="email">
+                    Email:  
+                    <input 
+                    id="email" 
+                    type="email" 
+                    name="email"
+                    //value=""
+                    //onChange= 
+                    placeholder="Please Enter your Email Address" />
+                    </label>
+                </inputDiv>
+
+                <inputDiv>
+                    <label htmlFor="password">
+                    Password:  
+                    <input 
+                    id="password" 
+                    type="password" 
+                    name="password"
+                    //value=""
+                    //onChange= 
+                    placeholder="Please Enter your Password" />
+                    </label>
+                </inputDiv>
+
+                <inputDiv>
+                    <h4>Our Terms &amp; Conditions</h4>
+                    <p>The following are the General Terms and Conditions under which Global Experience Specialists, Inc. (“GES”) provides Services to you, our valued customer ("Customer") under this Agreement and shall also apply to every Change Order or Show Site Services Order issued by GES to Customer. GES will not accept and hereby objects to any additional or different terms and conditions that may be contained in Customer’s purchase order or other writing. No modification or waiver of any provision in these General Terms and Conditions will be effective unless in writing and signed by the party sought to be charged with the change. GES’ performance is expressly conditioned on Customer’s acceptance of these General Terms and Conditions and Customer agrees to be bound hereby.</p>
+
+                    <label htmlFor="terms" className="terms">
+                    <input
+                    type="checkbox"
+                    name="terms"
+                    // checked=
+                    // onChange=
+                    />
+                    Terms and Conditions
+                    </label>
+                    
+                </inputDiv>
+
+                <inputDiv>
+                <button>
+                    Submit
+                </button>
+                </inputDiv>
+
+            </form>
+        </superContainer>
+      );
+    }
+
+
+export default Form;
