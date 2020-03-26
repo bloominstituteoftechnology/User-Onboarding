308124bfe0114ec08d627ecd33a60eec94cd584b
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-03-25 18:34:26 -0700

    MVP met

--- user_ob/src/component/Form.js
+++ user_ob/src/component/Form.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const formSchema = yup.object().shape({
     name: yup.string().required("Please enter your name :)"),
     email: yup.string().email().required("Don't forget to enter your email! How else can we send you tons of junk mail?!"),
-    userPassword: yup.string().password.required("Please add an overly complicated password, that you will most assuredly forget in the next 5 seconds."),
+    userPassword: yup.string().min(1).required("Please add an overly complicated password, that you will most assuredly forget in the next 5 seconds."),
     terms: yup.boolean().oneOf([true], "Please accept these terms and then we will teach you the secret handshake")
 })
 
@@ -14,7 +14,7 @@ export default function Form() {
     const [formState, setFormState] = useState({
         name: "",
         email: "",
-        password: "",
+        userPassword: "",
         terms: ""
     });
     const [errors, setErrors] = useState({
@@ -24,7 +24,9 @@ export default function Form() {
         terms: ""
     });
 
-    const [buttoonDisabled, setButtonDisabled] = useState(true);
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
 
@@ -51,88 +53,88 @@ export default function Form() {
                     [e.target.name]: err.errors[0]
                 });
             });
-
-        const formSubmit = e => {
-            e.preventDefault();
-            axios
-                .post("https://reqres.in/api/users", formState)
-                .then(res => {
-                    setPost(res.data);
-                    console.log("success", post);
-
-                    setFormState({
-                        name: "",
-                        email: "",
-                        password: "",
-                        terms: "",
-                    });
-                })
-                .catch(err => console.log(err.response));
-
-        };
-
-        const inputChange = e => {
-            e.persist();
-            const newFormData = {
-                ...formState,
-                [e.target.name]:
-                    e.target.type === "checkbox" ? e.target.checkbox :
-                        e.target.value
-            };
-            validateChange(e);
-            setFormState(newFormData)
-        };
-
-        return (
-            <form onSubmit={formSubmit}>
-                <label htmlFor="name">
-                    Name
-        <input
-                        id="name"
-                        type="text"
-                        name="name"
-                        value={formState.name}
-                        onChange={inputChange}
-                    />
-                    {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
-
-                </label>
-                <label htmlFor="email">
-                    Email
-          <input
-                        id="email"
-                        type="text"
-                        name="email"
-                        value={formState.email}
-                        onChange={inputChange}
-                    />
-                    {errors.email.length > 0 ? (
-                        <p className="error">{errors.email}</p>
-                    ) : null}
-                </label>
-                <label HTMLfor="userPassword">
-                    Password (8 characters minimum):
-        <input
-                        id="userPassword"
-                        type="password"
-                        value={formState.password}
-                        onChange={inputChange}
-                    />
-                    {errors.userPassword.length > 0 ? (
-                        <p className="error">{errors.userPassword}</p>
-                    ) : null}
-                </label>
-                <label HTMLfor="terms" className="terms">
-                    Terms of Service
-        <input
-                        type="checkbox"
-                        name="terms"
-                        checked={formState.terms}
-                        onChange={inputChange} />
-                </label>
-                <pre>{JSON.stringify(post, null, 2)}</pre>
-                <button>Submit</button>
-            </form>
-        );
     }
-      
+    const formSubmit = e => {
+        e.preventDefault();
+        axios
+            .post("https://reqres.in/api/users", formState)
+            .then(res => {
+                setUsers(users.concat(res.data));
+                console.log("success", users);
+
+                setFormState({
+                    name: "",
+                    email: "",
+                    userPassword: "",
+                    terms: "",
+                });
+            })
+            .catch(err => console.log(err.response));
+
+    };
+
+    const inputChange = e => {
+        e.persist();
+        const newFormData = {
+            ...formState,
+            [e.target.name]:
+                e.target.type === "checkbox" ? e.target.checkbox :
+                    e.target.value
+        };
+        validateChange(e);
+        setFormState(newFormData)
+    };
+    return (
+        <form onSubmit={formSubmit}>
+            <label htmlFor="name">
+                Name
+    <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    value={formState.name}
+                    onChange={inputChange}
+                />
+                {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
+
+            </label>
+            <label htmlFor="email">
+                Email
+      <input
+                    id="email"
+                    type="text"
+                    name="email"
+                    value={formState.email}
+                    onChange={inputChange}
+                />
+                {errors.email.length > 0 ? (
+                    <p className="error">{errors.email}</p>
+                ) : null}
+            </label>
+            <label HTMLfor="userPassword">
+                Password (8 characters minimum):
+                <input
+                    id="userPassword"
+                    type="password"
+                    name="userPassword"
+                    value={formState.userPassword}
+                    onChange={inputChange}
+                />
+                {errors.userPassword.length > 0 ? (
+                    <p className="error">{errors.userPassword}</p>
+                ) : null}
+            </label>
+            <label HTMLfor="terms" className="terms">
+                Terms of Service
+                <input
+                    type="checkbox"
+                    name="terms"
+                    checked={formState.terms}
+                    onChange={inputChange} 
+                />
+            </label>
+            <pre>{JSON.stringify(users, null, 2)}</pre>
+            <button disabled={buttonDisabled}>Submit</button>
+        </form>
+    );
+} 
