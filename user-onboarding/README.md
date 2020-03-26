77e15b6364bb8f00a8e65ba243a93aca573ea32a
Golden Eagle <joowoon.kang@biola.edu>
2020-03-25 21:28:19 -0700

    added styling for stretch by using styled component.

--- user-onboarding/README.md
+++ user-onboarding/README.md
@@ -1,68 +1,54 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Module Project: Advanced Form Management - User Onboarding
+## Project Description
 
-## Available Scripts
+We've seen many different styles of form management by now -- simple to complex. Today we are going to unleash your inner form-wizard! 🧙
 
-In the project directory, you can run:
+## Set Up The Project
 
-### `npm start`
+- [x] Start off by installing a blank React app by running `npx create-react-app <app-name> --use-npm`.
+- [x] Using `npm`, add the following as dependencies inside your React app:
+  - `yup`
+  - `axios`
+- [x] Create a component file called `Form.js`, import it into your `App.js` file, and place the component in your JSX there.
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## STEP 1 - Create Your Advanced Form
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+We want to create a form to onboard a new user to our system. We need _at least_ the following pieces of information about our new user:
 
-### `npm test`
+- [x] Name
+- [x] Email
+- [x] Password
+- [x] Terms of Service (checkbox)
+- [x] A Submit button to send our form data to the server.
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## STEP 2 - Implement Form Validation and Error Messaging
 
-### `npm run build`
+Form validation is one of the facets of an application that makes it feel polished and controlled from a user perspective. With that in mind, implement the following:
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+- [ ] Using Yup, set up _at least_ two different validations along with custom error messages that will display on screen when validation fails.
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+## STEP 3 - Make a POST Request
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Being able to `POST` data is a key skill of any developer, no matter your skill level.
 
-### `npm run eject`
+- [x] Craft a `POST` request using `axios` that sends your form data to the following endpoint: _https://reqres.in/api/users_
+- [x] Verify using a `console.log()` that you are receiving a successful response back
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+(Note: For those that are curious, we're using [reqres.in](https://reqres.in/) for this assignment's API. It's a free API that allows us to simulate a `POST` request for any data that we send it. Pretty awesome!)
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+## STEP 4 - Display Returned Data to Screen
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+When you get your data back, you will want to do something with it, right? Let's display a list of users in our app.
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+- [x] Set up a state property called `users` that is initialized with an empty array
+- [x] Every time you make a `POST` request, and get that new user data back, update your `users` state with the new user added to the array
+- [x] Render `users` in your app. You can <pre></pre> and JSON.stringify() method to display your post request.
 
-## Learn More
+## Stretch Goals
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+The following are stretch goals that you should attempt _after_ you meet MVP for your project:
 
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+- [ ] Add basic styling to your form in your app. Make it look pretty with any styling method you choose.
+- [ ] Implement a dropdown menu in your form. Add a `role` value to your state and add a dropdown with different roles for your users.
+- [ ] Create 3 new inputs inside your form of your choice along with corresponding validation and error messaging
+- [ ] Add to your existing handling so that, if a user inputs their email as `waffle@syrup.com`, they receive an error message in their form that says _"That email is already taken."_
--- user-onboarding/src/components/form.component.js
+++ user-onboarding/src/components/form.component.js
@@ -1,83 +1,210 @@
 import React, { useState, useEffect } from 'react'
+import axios from "axios"
+import * as yup from "yup"
+import styled from 'styled-components'
+import "./form.styles.css"
 
+
+
+
+const formSchema = yup.object().shape({
+    name: yup.string().required("Name is a required field."),
+    email: yup
+      .string()
+      .email("Must be a valid email address")
+      .required("Must include email address"),
+    password: yup.string().required("Password is a required field").min(4, "your password needs more 4 letters").matches(/(^(?=.*[!@#$%^&*]))/, "Your Password is required special characters"),
+    terms: yup.boolean().oneOf([true], "Please agree to terms of use"),
+    // positions: yup.string()
+})
+
+const Button = styled.button`
+/* Adapt the colors based on primary prop */
+    background: rgb(219, 112, 147);
+    color: white;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid rgb(219, 112, 147);
+    border-radius: 3px;
+`;
+
+
+const Body = styled.div`
+    display: flex;
+    align-items:center;
+    background-color: skyblue;
+    height: 20vh;
+    border: 2px solid grey;
+    width: 30%;
+    margin: 4% auto 0 auto;
+    padding: 3%;
+`;
+const Pre = styled.pre`
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    background-color: skyblue;
+    /* height: 50vh; */
+    border: 2px solid grey;
+    width: 30%;
+    margin: 3% auto;
+    padding: 3%;
+`
+
+
+// backgroundImage = "url('img_tree.png');
 
 const Form =(props) =>{
-    console.log("formprops",props)
-    const [formState, setFormState] = useState([{
+
+
+
+    const [formState, setFormState] = useState({
         name: "",
         email: "",
         password: "",
-        term:"",
-        position: ""
-    }]);
+        terms:"",
+        // position: ""
+    });
   
-    const [errors, setErrors] = useState([{
+    const [errors, setErrors] = useState({
         name: "",
         email: "",
         password: "",
-        term:"",
-        position: ""
-    }]);
+        terms:"",
+        // position: ""
+    });
+
+    const [post, setPost] = useState([]);
+
+    const [buttonDisabled, setButtonDisabled] = useState(true);
 
     useEffect(() => {
-
-    },[])
+        formSchema.isValid(formState)
+        .then(valid => {
+            setButtonDisabled(!valid);
+        })
+    },[formState])
        
 
-    const handleChange = event => {
-        setFormState({ 
-            ...formState, 
-            [event.target.name]: event.target.value });
-      };
+ 
 
     const handleSubmit = event => {
         event.preventDefault();
-        // console.log(formState.name);
-        // console.log(formState.email)
-        // console.log(formState.password)
-
-        props.addNewForm(formState);
-        setFormState({
-            name:"", 
-            email: "",
-            password:"",
-            term:"",
-            position: ""
-        });
+        axios
+            .post("https://reqres.in/api/users", formState)
+            .then(res => {
+                setPost([...post, res.data]);
+                console.log("success", post)
+           
+            setFormState({
+                name:"", 
+                email: "",
+                password:"",
+                terms:"",
+                // position: ""
+            });
+        })
+        .catch(err => console.log(err.response))
     }
-    console.log("form:",formState)
+
+    const validateChange = event => {
+        yup
+            .reach(formSchema, event.target.name)
+            .validate(event.target.value)
+            .then(valid =>{
+                setErrors({
+                    ...errors,
+                    [event.target.name]: ""
+                });
+            })
+            .catch(err => {
+                setErrors({
+                    ...errors,
+                    [event.target.name]: err.errors[0]
+                });
+            });
+    };
+    const handleChange = event => {
+        event.persist();
+        const newFormData= {
+            ...formState,
+            [event.target.name]:
+                event.target.type === "checkbox" ? event.target.checked
+                    : event.target.value
+        }
+        validateChange(event);
+        setFormState(newFormData)
+
+      };
+      console.log(errors)
     return (
         <div>
-     
-        <form onSubmit={handleSubmit}>
-            <label>Full Name</label>
+         <Body >
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="name">
+            Full Name
                 <input 
                     type="text"
                     name="name"
                     value={formState.name}
                     onChange={handleChange}
-                    
                 />
-                 <br />           
-            <label>Email</label>   
+                {errors.name.length > 0 ? <p className="error">
+                {errors.name}</p> : null}
+                </label>
+                 <br />     
+
+            <label htmlFor="email">
+                 Email
                 <input 
                     type="text"
                     name="email"
                     value={formState.email}
                     onChange={handleChange}
                 />
+                {errors.email.length > 0 ? (
+                    <p className="error">{errors.email}</p>
+                ) : null }
+                </label>   
                   <br />    
-            <label>password</label>
+
+            <label htmlFor="password">
+            password
                 <input 
-                    type="text"
+                    type="password"
                     name="password"
                     value={formState.password}
                     onChange={handleChange}
                 />
-                  <br />    
-            <button>Submit!</button>
+                  {errors.password.length > 0 ? (
+                    <p className="error">{errors.password}</p>
+                ) : null }
+                </label>
+                  <br />
+            <label htmlFor="terms">
+                Terms & Condition
+                <input 
+                    type="checkbox"
+                    name="terms"
+                    checked={formState.terms}
+                    onChange={handleChange}
+                />
+                  {errors.terms = false ? (
+                    <p className="error">{errors.terms}</p>
+                ) : null }
+            </label>
+                    <br/>
+            <Button disabled={buttonDisabled}>Submit!</Button>
+
+           
         </form>
+        </Body>
+            <Pre>{JSON.stringify(post, null, 2)}</Pre>
         </div>
+       
+     
+        
     )
 }
 
