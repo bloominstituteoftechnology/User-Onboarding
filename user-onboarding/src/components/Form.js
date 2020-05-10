13b6ca42bcd3defb0620942c127d9c64a61eacdd
Ben Venker <bvemails@gmail.com>
2020-05-10 15:00:05 -0500

    set up basic form

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
+import { css, cx } from "emotion";
 
 const Form = () => {
-  const defuaultState = {
+  const defaultState = {
     name: "",
     email: "",
     password: "",
     terms: false,
   };
 
-  const [formState, setFormState] = useState(defuaultState);
+  const [formState, setFormState] = useState(defaultState);
 
   const submitForm = (e) => {
     e.preventDefault();
@@ -21,12 +22,44 @@ const Form = () => {
   };
 
   return (
-    <form onSubmit={submitForm}>
+    <form
+      onSubmit={submitForm}
+      className={css`
+        width: 33%;
+        margin: auto;
+      `}
+    >
       <label htmlFor="name">
         Name
-        <input type="text" name="name" />
+        <input type="text" name="name" className={css``} />
       </label>
-      <button>Submit</button>
+      <label htmlFor="email">
+        Email
+        <input type="email" name="email" className={css``} />
+      </label>
+      <label htmlFor="name">
+        Password
+        <input type="password" name="password" className={css``} />
+      </label>
+      <label htmlFor="name">
+        What are your career goals?
+        <textarea name="motivation" className={css``} />
+      </label>
+      <label htmlFor="role">
+        What role do you currently have?
+        <select name="roles" className={css``}>
+          <option value="engineer">Engineer</option>
+          <option value="manager">Manager</option>
+          <option value="operations">Operations</option>
+          <option value="finance">Finance</option>
+        </select>
+      </label>
+      <label htmlFor="terms">
+        <input type="checkbox" name="terms" checked={true} />
+        Terms & Conditions
+      </label>
+
+      <button type="submit">Submit</button>
     </form>
   );
 };
