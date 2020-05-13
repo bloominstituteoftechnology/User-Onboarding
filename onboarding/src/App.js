8d9fc07a115a42e1f8027b88bc7e41a97a7fb086
unknown <clmonitor2013@gmail.com>
2020-05-12 21:22:09 -0400

    first-commit

---
+++ onboarding/src/App.js
@@ -0,0 +1,16 @@
+import './css/index.css';
+import React from 'react';
+import Header from './components/header/Header';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ onboarding/src/components/header/Header.js
@@ -0,0 +1,32 @@
+import '../../css/index.css';
+
+import React , {useState} from 'react';
+import { useHistory } from "react-router-dom";
+import { Link } from 'react-router-dom'
+
+const Header = props => {
+  
+  console.log(window.location.pathname);
+  return (
+    
+      <div className="header" >
+        <h3>My Lambda Team (webpt16)</h3>
+        
+        <Link to="/add">
+          <div className="button">Add Member</div>
+          
+        </Link>
+        <Link to="/" >
+          <div className="button">Home</div>
+          
+        </Link>
+
+
+      </div>
+    
+  );
+
+  
+}
+
+export default Header;
