5deb1c574ff4f091aa2e2226d7e0940fb1c4e990
Joey <joey.d.wilson@gmail.com>
2020-05-21 16:04:56 -0400

    Update form_test.js

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,11 +1,3 @@
-//  Get the Name input and type a name in it.
-//  Use an assertion to check if the text inputted contains the name you provided (Hint: use the .should assertion)
-//  Get the Email input and type an email address in it
-//  Get the password input and type a password in it
-
-//  Set up a test that will check to see if a user can check the terms of service box
-//  Check to see if a user can submit the form data
-//  Check for form validation if an input is left empty
 
 describe("Form inputs", () => {
   it("can navigate to the site", () => {
