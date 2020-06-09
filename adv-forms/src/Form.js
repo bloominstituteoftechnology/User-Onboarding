4b3cbebbe07e859f57440c09509d97206c9943cc
JenVest2020 <jenvest2019@gmail.com>
2020-06-09 18:30:59 -0500

    yup validation set/working on error messages

--- adv-forms/src/Form.js
+++ adv-forms/src/Form.js
@@ -16,6 +16,34 @@ const FormDiv = styled.div`
 `;
 
 function Form() {
+    const [formData, setFormData] = useState([]);
+    const [isButtonDisabled, setisButtonDisabled] = useState(true);
+    const [errors, setErrors] = useState({
+        name: '',
+        email: '',
+        password: '',
+        terms: ''
+    });
+
+    const formSchema = yup.object().shape({
+        name: yup.string().required('Name is a required field'),
+        email: yup.string().required('Must be a valid email address'),
+        password: yup.string().required('please enter your password')
+            .matches(
+                "^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$",
+                "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+            ),
+        terms: yup.boolean().oneOf([true], 'please, agree to our Terms of Service'),
+    });
+
+    useEffect(() => {
+        formSchema.isValid(formData)
+            .then(valid => {
+                console.log('valid?', valid);
+                setisButtonDisabled(!valid);
+                setErrors(formData);
+            })
+    }, [formData])
 
 
     return (
