8b0539c6f610f2c94a0b7863d28cb056f18fa061
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-06-17 16:22:44 -0700

    mvp

---
+++ user-onboarding/src/components/App.js
@@ -0,0 +1,164 @@
+import React, { useState, useEffect } from 'react';
+import User from './User'
+import UserForm from './UserForm'
+import FormSchema from './validation/FormSchema'
+import axios from 'axios'
+import * as Yup from 'yup'
+import '../App.css'
+
+
+const initialFormValues = {
+  first_name: '',
+  last_name: '',
+  email: '',
+  password: '',
+  tos: {
+    tos: false,
+  }
+}
+
+const initialFormErrors = {
+  first_name: '',
+  last_name: '',
+  email: '',
+  password: '',
+  tos: {
+    tos: false,
+  }
+}
+
+const initialUsers = []
+const initialDisabled = true
+
+function App() {
+  //////////////// STATES ////////////////
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  //////////////// HELPERS ////////////////
+  const getUsers = () => {
+    axios.get('https://reqres.in/api/users')
+      .then(res => {
+        setUsers(res.data.data)
+      })
+      .catch(err => {
+        console.log(err)
+        debugger
+      })
+  }
+
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(res => {
+        setUsers([...users.data, res.data.data])
+      })
+      .cath(err => {
+        debugger
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
+  }
+
+  //////////////// EVENT HANDLERS ////////////////
+  const onInputChange = evt => {
+    const name = evt.target.name
+    const value = evt.target.value
+    Yup
+      .reach(FormSchema, name)
+      .validate(value)
+      .then(valid => {
+        setFormErrors({
+          ...formErrors,
+          [name]: ""
+        });
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0]
+        });
+      });
+
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const onCheckboxChange = evt => {
+    const name = evt.target.name
+    const checked = evt.target.checked
+    console.log()
+    Yup
+      .reach(FormSchema, name)
+      .validate()
+      .then(valid => {
+        setFormErrors({
+          ...formErrors,
+          [name]: ''
+        });
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0]
+        });
+      });
+    setFormValues({
+      ...formValues,
+      tos: {
+        ...formValues.tos.tos,
+        [name]: checked,
+      }
+    })
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+
+    const newUser = {
+      first_name: formValues.first_name.trim(),
+      last_name: formValues.last_name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      tos: formValues.tos // this needs checking 
+    }
+    postNewUser(newUser)
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  useEffect(() => {
+    FormSchema.isValid(formValues).then(valid => {
+      setDisabled(!valid);
+    })
+  }, [formValues])
+
+  return (
+    <div className='app'>
+      <h1>Users App</h1>
+      <UserForm
+        values={formValues}
+        onInputChange={onInputChange}
+        onCheckboxChange={onCheckboxChange}
+        onSubmit={onSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+      {
+        users.map(user => {
+          return (
+            <User key={user.id} details={user} />
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
