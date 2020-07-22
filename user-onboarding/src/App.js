a7cc04bf5fd21557130f3f9365ad16fe39bb41c7
Ashley <ashley.bergsma42@gmail.com>
2020-07-22 16:15:06 -0700

    second commit

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
+import axios from 'axios'; 
+import * as yup from 'yup'; 
 import Form from './Form';
 import logo from './logo.svg';
 import './App.css';
 
+// INITIAL STATES // 
+// Form Values // 
+const initialFormValues = {
+  name: '',
+  email: '', 
+  password: '', 
+  ToS: {
+    read: false;
+  }
+}
+// Form errors // 
+const initialFormErrors = {
+  name: '', 
+  email: '', 
+  password: '', 
+}
+// Member list (array) and button disabled // 
+const initialMembers = []; 
+const initialDisabled = true;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -3,8 +3,17 @@ import React from 'react';
 export default function Form() {
 
     return(
-        <form>
-            
+        <form className='form-wrapper'>
+            <label htmlFor='nameInput'>
+                Name:
+                <input
+                id='nameInput'
+                name='name'
+                type='text'
+                placeholder='Enter Your Name'
+                maxLength='25'
+                />
+            </label>
         </form>
     )
 }
