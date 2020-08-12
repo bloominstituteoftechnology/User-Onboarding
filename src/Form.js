f2dc7138e84d8112cb616df1a489c3aeb5d589ea
Daniel Terry <daniel.terry2121@gmail.com>
2020-08-11 23:24:27 -0400

    Form basic layout

--- src/Form.js
+++ src/Form.js
@@ -1,43 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
+import * as yup from 'yup';
 
 const Form = () => {
+ //setting state for form data
+
+const [formData, setFormData] = useState({
+    name: '',
+    email:'',
+    password:'',
+    terms: true
+});
+//state for button
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+//change handler
+    const change = event => {
+        const newFormData = {
+            ...formData, 
+            [event.target.name]: event.target.type === 'checkbox' ? event.target.checked: event.target.value
+        }
+        setFormData({newFormData});
+    }
+//submit function
+const submitFunction = event => {
+    event.preventDefault();
+}
     return(
         <form>
             <label htmlFor = "name">
                 Name
                 <input type = "text" 
                        name = "name"
-                       value = ''
-                       onChange = ''
+                       value = {formData.name}
+                       onChange = {change}
                 />
             </label>
             <label htmlFor = "email">
                 Email
                 <input type = "email" 
                        name = "email"
-                       value = ''
-                       onChange = ''
+                       value = {formData.email}
+                       onChange = {change}
                 />
             </label>
             <label htmlFor = "password">
                 Password
                 <input type = "password" 
                        name = "password"
-                       value = ''
-                       onChange = ''
+                       value = {formData.password}
+                       onChange = {change}
                 />
             </label>
             <label htmlFor = "terms">
                 Terms of Service
                 <input type = "checkbox" 
                        name = "terms"
-                       checked = ''
-                       onChange = ''
+                       checked = {formData.terms}
+                       onChange = {change}
                 />
             </label>
-            <button type = "submit">Submit</button>
+            <button disabled = {buttonDisabled} type = "submit">Submit</button>
         </form>
-    )
+    );
 }
 
 export default Form;
