6b32ad655dec2197720a366ae0d3f76f9f22fc51
shenandoah-veele <shenandoahjeroline@gmail.com>
2020-08-12 20:04:57 -0400

    mvp meet

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as yup from 'yup';
+import axios from 'axios';
 
+//setting up yup 
+
+const formSchema = yup.object().shape({
+    name: yup.string().required("Name is a required field"),
+    email: yup.string().email("Must include a valid email").required("Please enter a valid email address"),
+    password: yup.string().required("Please enter password"),
+    terms: yup.boolean().oneOf([true], "Please agree to Terms and Conditions")
+})
 
 function Form() {
 
@@ -12,49 +22,95 @@ function Form() {
         terms: false
         });
 
+    
+    //button disable
+
+    const [buttonDisabled, setButtonDisabled] = useState("");
+    // setting up errors
+
+    const [errorState, setErrorState] = useState({
+        name: "",
+        email:"",
+        password:"",
+        terms: ""  
+    })
     // onSubmit Function
 
-    const formSubmit =e => {
+    const formSubmit = (e) => {
         e.preventDefault();
         console.log("form submitted!");
+        axios.post('https://reqres.in/api/users', formState)
+        .then(response => console.log(response),"success!!!")
+        .catch(err => console.log(err),"Failed :(");
     };
 
+    // validating form
+
+    const validate = (e) => {
+       yup.reach(formSchema, e.target.name)
+        .validate(e.target.value)
+        .then( valid => {
+            setErrorState({
+                ...errorState,
+                [e.target.name]: ""
+            })
+
+        }) 
+        .catch(err => {
+            console.log(err.errors)
+            setErrorState({
+                ...errorState, 
+                [e.target.name]: err.errors[0]
+            })
+        }) 
+    }
+        
     // onChange Function 
 
     const inputChange = e => {
-        console.log("input changed!", e.target.name, e.target.checked);
+        e.persist()
+        validate(e)
         let value = e.target.type === "checkbox" ? e.target.checked : e.target.value
         setFormState({...formState, [e.target.name]: value });
     };
+
+    useEffect(() => {
+        formSchema.isValid(formState)
+        .then((isValid) => {
+            setButtonDisabled(!isValid);
+        });
+    }, [formState]);
 
     return(
         <form onSubmit ={formSubmit}>
             <label htmlFor="name">
                 Name    
                 <input type="text" name="name" id="name" placeholder="Please Enter Name" value={formState.name} onChange={inputChange} />
+                {errorState.name.length > 0 ? <p classname="error">{errorState.email}</p> : null}
             </label>
             <br></br>
             <label htmlFor="email">
-                email
-                <input type="email" name="email" id="email" placeholder="Please Enter Email" value={formState.email} onChange={inputChange} /> 
+                Email
+                <input type="email" name="email" id="email" placeholder="Please Enter Email" value={formState.email} onChange={inputChange} />
+                {errorState.email.length > 0 ? <p className="error">{errorState.email}</p> : null} 
             </label>
             <br></br>
             <label htmlFor="password">
                 Password
                 <input type="password" name="password" id="password" placeholder="Please Enter password" value={formState.password} onChange={inputChange} />
+                {errorState.email.length > 0 ? <p className="error">{errorState.password}</p> : null}            
             </label>
             <br></br>
             <label htmlFor="Terms of Service">
                 Do you agree to the Terms of Service?
                 <input type="checkbox" name="terms" id="terms" checked={formState.terms} onChange={inputChange} />
+            {errorState.terms.length > 0 ? <p className="error">{errorState.terms}</p> : null}    
             </label>
             <br></br>
-            <button>Submit</button>
+            <button disabled={buttonDisabled} type="submit">
+                Submit
+            </button>
         </form>
-
-
-
-                   
 
     );
 }
