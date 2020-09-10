70b19a25503bdb9aa873c6eacaa8979094f1c43b
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-09-10 19:11:12 -0400

    Had to delete and restart final project

---
+++ app-name/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from "./Form"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{backgroundColor: "#14213d"}}>
+      <h1 style={{padding: "2%", color: "#fca311"}}>Onboard New User Form</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
