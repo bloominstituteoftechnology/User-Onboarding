f4f06102f733d09c319d63b474cb43e1a1ed7f1d
karmaeiic <46303520+karmaeiic@users.noreply.github.com>
2020-09-13 14:17:55 -0700

    testing

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,20 +1,61 @@
-describe('My First test', function () {
-    it ('not much', () => {
-        cy.visit('/Form')
-        
-        cy.get('form > :nth-child(1) > label > input')
-        .type('Daniel')
-        .should('have.value','Daniel')
-
-        cy.get(':nth-child(2) > label > input')
-        .type('karmaeiic.neuushi@gmail.com')
-        .should('have.value','karmaeiic.neuushi@gmail.com')
-
-        cy.get(':nth-child(3) > label > input')
-        .type('password')
-        .should('have.value','password')
-
-        cy.get('[type="checkbox"]')  .check({ force: true }).should('be.checked')  
-
-        cy.get('form').submit()
+describe('Form inputs', () => {
+    it('Navigates to App', () => {
+        cy.visit('http://localhost:3000/')
+        cy.url()
+        .should('include', 'localhost')
     })
+
+    
+
+    it('Adds a Name', () => {
+        cy.get('input[name="name"]')
+            .type('Beggs')
+            .should('have.value', "Beggs")
+    })
+
+    it('Adds a Password', () => {
+        cy.get('input[name="password"]')
+            .type('PASSWORD')
+            .should('have.value', "PASSWORD")
+    })
+
+    it('Adds a Email', () => {
+        cy.get('input[name="email"]')
+            .type('karmaeiic.neuushi@gmail.com')
+            .should('have.value', "karmaeiic.neuushi@gmail.com")
+    })
+
+    it('Checks Terms Box', () => {
+        cy.get('input[name="terms"]')
+            .click()
+            // .should('Be checked')
+    })
+    it('Submits inputs', () => {
+        cy.get('button.submit')
+            .should('not.be.disabled')
+    })
+})
+
+describe('wont submit with missing inputs', () => {
+    it('wont submit when email is missing', () => {
+        cy.visit('http://localhost:3000/')
+
+        cy.get('input[name="name"]').type('Beggs')
+        cy.get('input[name="password"]').type('PASSWORD')
+        cy.get('input[name="terms"]').click()
+        cy.get('button.submit').should('be.disabled')
+
+    })
+})
+
+describe('Submit a user', () =>{
+    it('can submit a user', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('input[name="name"]').type('Daniel Beggs')
+        cy.get('input[name="email"]').type('daniel.micheal.beggs@gmail.com')
+        cy.get('input[name="password"]').type('PASSWORD')
+        cy.get('input[name="term"]').click()
+        cy.get('button.submit').click()
+
+    })
+})
