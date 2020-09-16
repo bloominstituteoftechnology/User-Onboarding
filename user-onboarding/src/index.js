c75a23e41108a17dcddf503d8f0fd2e83d27ece6
Simone Ballard <simone.a.ballard@gmail.com>
2020-09-16 17:34:06 -0600

    pre tag added into index.js

--- user-onboarding/src/index.js
+++ user-onboarding/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from '..components/App';
 import * as serviceWorker from './serviceWorker';
 
+<pre>{JSON.stringify(post, null, 2)}</pre>
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
