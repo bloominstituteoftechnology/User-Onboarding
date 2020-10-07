2e2fda8e9d098a328f66ab8766195d813f97d825
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-10-06 17:50:43 -0700

    Created form and set the state for the form inputs

---
+++ useronboarding/src/component/Form.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react"
+
+
+
+export default function Form() {
+//manages state
+const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    terms: false
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
