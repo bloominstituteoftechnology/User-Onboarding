9314f714743f924f196cbe7acf700322925a7716
Joe L <joseph.lasata@gmail.com>
2020-10-14 14:11:34 -0500

    React App Up and Running - Components Created

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Form from './Form'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Onboarding App!</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Form() {
+  return (
+    <div className="Form">
+      <h2>User Onboarding Form</h2>
+    </div>
+  );
+}
+
+export default Form;
