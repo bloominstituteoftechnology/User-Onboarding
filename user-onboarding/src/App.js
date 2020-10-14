d40c7e4d2d4f03e1faf4c01fa150de55cdb8d04e
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-14 15:57:06 -0500

    Planning out basic values.

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Form from "./components/Form"
 
+const initialValues = {
+  //Text fields
+  name: "",
+  email: "",
+  password: "",
+  //Checkbox
+  terms: false,
+}
+
+
 function App() {
   return (
     <div className="App">
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+//We need: 
+//*Name (text field)
+//*Email (text field)
+//*Password (text field)
+//*Terms of Service (checkbox)
+//*Submit button (button)
 
 export default function Form(){
 
