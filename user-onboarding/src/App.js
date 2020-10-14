941e024d671c2eaa3433ee22d5ee57d06cb44ca5
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-14 16:30:43 -0500

    Adding layout to Form.js

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from "./components/Form"
+import axios from "axios";
+//you'll probably still have to import *yup*, and maybe the *schema* if you have to create one
 
 const initialValues = {
   //Text fields
@@ -11,9 +13,14 @@ const initialValues = {
   //Checkbox
   terms: false,
 }
+const initialUsers = [];
+const initialDisabled = true;
 
+export default function App() {
+  const [users, setUsers] = useState(initialUsers);
+  const [disabled, setDisabled] = useState(initialDisabled);
+  const [userValues, setUserValues] = useState(initialValues);
 
-function App() {
   return (
     <div className="App">
      <Form />
@@ -21,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -7,9 +7,54 @@ import React from 'react'
 //*Terms of Service (checkbox)
 //*Submit button (button)
 
-export default function Form(){
+export default function Form(props){
+    const { values, submit, change, disabled } = props;
+
+    //onSubmit functionality
+    const onSubmit = (event) => {
+        event.preventDefault();
+        submit();
+    };
+
+    //onChange functionality
+    const onChange = (event) => {
+        const { name, value, type, checked } = event.target;
+        const valueToUse = type === "checkbox" ? checked : value;
+        change(name, valueToUse);
+    };
 
     return (
-        <h2>User Onboarding Form!</h2>
+        <form className="formContainer" onSubmit={onSubmit}>
+            <div className="formHeading">
+                <h2>User Onboarding Form!</h2>
+                {/* errors would go here, I think */}
+            </div>
+
+            <div className="formInputs">
+                <h4>Please Fill Out Form Below:</h4>
+                {/* Text Inputs */}
+                    <label>
+                        Name: <input 
+                        value={values.name}
+                        onChange={onChange}
+                        name="name"
+                        type="text"
+                        />        
+                    </label>
+
+                    <label>
+                        Email: <input 
+                        value={values.email}
+                        onChange={onChange}
+                        name="email"
+                        type="text"
+                        />        
+                    </label>
+                {/* Checkbox */}
+            </div>
+                
+                {/* Submit button */}
+                <button disabled={disabled}>Finalize</button>
+        </form>
     )
 }
