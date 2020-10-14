94a8eeeb0f8eec66f739f189420a6ce7b3ec8624
Joe L <joseph.lasata@gmail.com>
2020-10-14 17:00:25 -0500

    Form Built out and functional

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,91 @@
-import React from 'react';
-import './App.css';
-import Form from './Form'
+import React, { useState } from 'react';
+import '../App.css';
+import Form from './Form';
+import axios from 'axios';
+
+//Define some needed varibles to reset state to baseline
+const initialUsers = [];
+const initialFormValues = {
+  name: "",
+  email: "",
+  password: "",
+  /// Checkbox - so we're rocking the bools ///
+  tos: false,
+};
+const initialFormErrors = {
+  name: "",
+  email: "",
+  password: "",
+}
 
 function App() {
+
+  //Grab the necessary Slices
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+  //Some Helpers for us - lets stay DRY
+  const postNewUser = (newUser) => {
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(res => {
+        setUsers([res.data, ...users]);
+        setFormValues(initialFormValues);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  //Event Handlers
+  const inputChange = (name, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const formSubmit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      tos: formValues.tos,
+    };
+    postNewUser(newUser);
+  }
+
   return (
     <div className="App">
       <h1>User Onboarding App!</h1>
-      <Form />
+      <Form 
+        values={formValues}
+        errors={formErrors}
+        change={inputChange}
+        submit={formSubmit}
+      />
     </div>
   );
 }
 
 export default App;
+
+
+/* Lets Plan some things here: 
+
+Create Form 
+ - Form should accept: Name, Email, Password, Terms of Service Checkbox, Submit Button
+ - Enable/ Disable submit button based on Form Validation 
+
+Form Validation & Error Messaging
+  - Use Yup to set up validations w/ custom error messages to display on the screen 
+
+Post Request: 
+  - Use axios post request to send form data to endpoint 'https://reqres.in/api/users'
+  -Verify we're getting a response back (console.log(res))
+
+Display Returned Data to Screen: 
+  - Slice some State for users 
+  - When we post and get new user data back - setUser(res, ...users), update users
+  - Render users in app
+*/
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,74 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 
-function Form() {
-  return (
-    <div className="Form">
-      <h2>User Onboarding Form</h2>
-    </div>
-  );
+function Form(props) {
+
+    const {values, errors, change, submit} = props;
+
+    const onSubmit = (evt) => {
+        evt.preventDefault();
+        submit();
+    };
+
+    const onChange = (evt) => {
+        const { name, value, type, checked } = evt.target;
+        const valueToUse = type === "checkbox" ? checked : value;
+        change(name, valueToUse);
+    };
+
+    return (
+    <form className="form-container" onSubmit={onSubmit}>
+        <div className="form-group submit">
+            <h2>User Onboarding Form</h2>
+            <button>Submit!</button>
+            <div className="errors">
+                {/* Display Errors here */}
+            </div>
+
+        </div>
+
+        <div className="form-group inputs">
+            <label>
+                Name: 
+                <input 
+                    value={values.name}
+                    onChange={onChange}
+                    name="name"
+                    type="text"
+                />
+            </label>
+            <label>
+                Email: 
+                <input 
+                    value={values.email}
+                    onChange={onChange}
+                    name="email"
+                    type="text"
+                />
+            </label>
+            <label>
+                Password: 
+                <input 
+                    value={values.password}
+                    onChange={onChange}
+                    name="password"
+                    type="text"
+                />
+            </label>
+        </div>
+        <div className="form-group checkbox">
+            <label> I have read and agree to the Terms of Service
+                <input 
+                    type="checkbox"
+                    name="tos"
+                    checked={values.tos}
+                    onChange={onChange}
+                />
+            </label>
+        </div>
+    </form>
+    );
+ 
 }
 
 export default Form;
