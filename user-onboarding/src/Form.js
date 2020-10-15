d89dce3b2ae1b2e452f353882fa77cc82555d75c
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-10-15 07:46:39 -0400

    mvp

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,7 +1,72 @@
 import React from 'react';
 
-function Form () {
-    return null;
+function Form(props) {
+    const { values, submit, change, disabled, errors } = props;
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        submit();
+    };
+
+    const onChange = (event) => {
+        const { name, value, type, checked } = event.target;
+        const valueToUse = type === "checkbox" ? checked : value;
+        change(name, valueToUse);
+    };
+    
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <button disabled={disabled} type="submit">Submit</button>
+                <div>
+                    <div>{errors.name}</div>
+                    <div>{errors.email}</div>
+                    <div>{errors.password}</div>
+                </div>
+            </div>
+            <div>
+                <label>
+                    Name:
+                    <input
+                        value={values.name}
+                        onChange={onChange}
+                        name="name"
+                        type="text"
+                    />
+                </label>
+                <label>
+                    Email:
+                    <input 
+                        value={values.email}
+                        onChange={onChange}
+                        name="email"
+                        type="text"
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input 
+                        value={values.password}
+                        onChange={onChange}
+                        name="password"
+                        type="text"
+                    />
+                </label>
+                <label>
+                    Terms of Service
+                    <input 
+                        type="checkbox"
+                        name="terms"
+                        checked={values.terms}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+        </form>
+
+        ///User Card///
+
+    );
 }
 
 export default Form;
