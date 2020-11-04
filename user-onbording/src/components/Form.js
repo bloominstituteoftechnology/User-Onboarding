7748ff6d03a72e0e591157febe71071ac1826fc2
Lakendria <kendra939@gmail.com>
2020-11-03 21:00:25 -0500

    styled form

--- user-onbording/src/components/Form.js
+++ user-onbording/src/components/Form.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import {Container, Col,Label,FormGroup, Input, Button} from "reactstrap";
 
 export default function Form (){
     // managing state for our form inputs
@@ -6,66 +7,85 @@ export default function Form (){
         name:"",
         email:"",
         password:"",
-        termsOfService:""
-
+        terms:false
     });
+
 // onSubmit function
     const formSubmit = e => {
         e.preventDefault();
         console.log("form submitted!");
+ 
     };
 
     //onChange function 
     const inputChange = e =>{
-        console.log("input changed!", e.target.value);
-        setUser({name:e.target.value})
+        console.log("input changed!", e.target.value, e.target.checked);
+        let value = e.target.type === "checkbox" ? e.target.checked : e.target.value
+        setUser({...user, [e.target.name]: value});
     };
 
     return (
+        <Container>
         <form onSubmit={formSubmit}>
-            <label htmlFor="name">
+            <Col>
+            <FormGroup>
+            <Label htmlFor="name">
                 Name:
-                <input type="text" 
+                <Input type="text" 
                 name="name" 
                 id="name"
                 placeholder = "Name"
                 value={user.name}
                 onChange={inputChange}
-                />
-            </label>
-
-            <label htmlFor="email">
+                />   
+            </Label>
+            </FormGroup>
+        
+            </Col>  
+              <FormGroup>
+            <Col>
+            <Label htmlFor="email">
                 Email:
-                <input type="email" 
+                <Input type="email" 
                 name="email" 
                 id="email"
                 placeholder = "Email"
                 value={user.email}
                 onChange={inputChange}
                 />
-                </label>
-
-                <label htmlFor="password">
+                </Label>
+                </Col> 
+                 </FormGroup>
+                <Col>
+                <FormGroup>
+                <Label htmlFor="password">
                 Password:
-                <input type="password" 
+                <Input type="password" 
                 name="password" 
                 id="password"
                 placeholder = "Password"
                 value={user.password}
                 onChange={inputChange}
                 />
-            </label>
-            <label htmlFor="Terms of Service">
+            </Label>
+            </FormGroup>
+            </Col>
+            
+            <Col>
+            <Label htmlFor="Terms of Service">
                 Terms of Service:
-                <input type="checkbox" 
-                name="terms of service" 
-                id="terms of service"
-                value={user.termsOfService}
+                <Input 
+                type="checkbox" 
+                name="terms" 
+                id="terms"
+                checked={user.terms}
+                onChange = {inputChange}
                 />
-            </label>
-           
-            <button>Submit</button>
+            </Label>
+           </Col>
+            <Button>Submit</Button>
 
         </form>
+        </Container>
     )
 }
