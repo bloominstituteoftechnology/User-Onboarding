980f41a3c2483cf7789ad7b4ded3a2ee66e5f0a1
cmirza <mail@cmirza.com>
2020-11-12 17:00:09 -0800

    fixed Cypress validation test

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -2,7 +2,7 @@ describe('Testing Form Inputs', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/');
     });
-    it('Lets test some inputs...', function() {
+    it('Lets test some inputs...', () => {
         cy.get('[data-cy=name]')
         .type('Cameron Mirza')
         .should('have.value', 'Cameron Mirza');
@@ -23,3 +23,20 @@ describe('Testing Form Inputs', () => {
         .click()
     })
 })
+
+describe('Testing Form Validation', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/');
+    });
+    it('Lets test a validation error on missing checkbox', () => {
+        cy.get('[data-cy=name]').type('Cameron Mirza')
+        cy.get('[data-cy=email]').type('mail@cmirza.com')
+        cy.get('[data-cy=password]').type('supersecretpassword')
+        
+        cy.get('[data-cy=checkbox]').check()
+        cy.get('[data-cy=checkbox]').uncheck()
+        
+
+        cy.get('[data-cy=warning]').should('have.text', 'Must accept terms!')
+    })
+});
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -159,7 +159,7 @@ const Form = () => {
         <div>
             <StyledForm onSubmit={ formSubmit }>
                 <div className ='warning'>
-                    <div>{errors.name}{errors.email}{errors.password}{errors.terms}</div>
+                    <div data-cy='warning'>{errors.name}{errors.email}{errors.password}{errors.terms}</div>
                 </div>
                 <div className='inputItem'>
                     <label>Name: </label>
