44350abcdf189eb5b214d452eeb9f73801f4e242
Elijah <elijahoneal@yahoo.com>
2020-12-08 22:43:43 -0500

    React Form Data + Yup Validation

---
+++ onboarding/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>React Form</h1>
+      </header>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ onboarding/src/components/Form.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import * as Yup from "yup";
+
+const formSchema = Yup.object().shape({
+    name: Yup
+        .string()
+        .required("Name is required"),
+    email: Yup
+        .string()
+        .email("Must be a valid email address.")
+        .required("Must include email address."),
+    password: Yup
+        .string()
+        .required("Password is Required")
+        .min(6, "Passwords must be at least 6 characters long"),
+    terms: Yup
+        .boolean()
+        .oneOf([true], "Give me the data.")
+   
+});  
+
+const Form = () => {
+    const [userData , setUserData] = useState({ name: "", email: "", password: "", terms: false});
+    const [errors , setErrors] = useState({ name: "", email: "", password: "", terms:""});
+    const [disabled , setDisabled] = useState(true);
+
+    const setFormErrors = (name , value) => {
+        Yup.reach(formSchema , name).validate(value)
+        .then( () => setErrors({...errors , [name]: ""}))
+        .catch(err => setErrors({...errors , [name]: err.errors[0] }))
+    }
+
+    const handleChange = event => {
+        const { name, type, value, checked } = event.target;
+        const updateInfo = type === 'checkbox' ? checked : value;
+        setFormErrors(name ,updateInfo )
+        setUserData({...userData , [name]: updateInfo});
+    };
+
+    const submitForm = (event) => {
+        event.preventDefault();
+        console.log(userData)
+    
+      }
+
+    useEffect( () => {
+        formSchema.isValid(userData).then(valid => setDisabled(!valid))
+    },[userData])
+
+    return (
+        <div className="errorList">
+            <div>{errors.name}</div>
+            <div>{errors.email}</div>
+            <div>{errors.password}</div>
+            <form onSubmit = {submitForm}>
+            <label htmlFor = "name">Name</label>
+                <input
+                id="name"
+                name = "name"
+                value = {userData.name}
+                type = "text"
+                placeholder = "Enter Name"
+                onChange = {handleChange}
+                />
+            
+            <label htmlFor = "email">Email</label>
+                <input
+                id="email"
+                name = "email"
+                value = {userData.email}
+                type = "text"
+                placeholder = "Enter Email"
+                onChange = {handleChange}
+                />
+
+            <label htmlFor = "password">Password</label>
+                <input
+                id="password"
+                name = "password"
+                value = {userData.password}
+                type = "text"
+                placeholder = "Enter Password"
+                onChange = {handleChange}
+                />
+            
+
+            <label htmlFor ="terms">Terms And Conditions</label>
+                <input
+                id="terms"
+                name = "terms"
+                type = "checkbox"
+                checked = {userData.terms}
+                value = "terms"
+                placeholder = "Terms And Conditions"
+                onChange = {handleChange}
+                />
+            
+
+            <button disabled={disabled} type = "submit">Submit</button>
+        </form>
+        </div>
+
+        
+    )
+}
+
+export default Form;
