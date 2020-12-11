84e5a1286183c78570a0f07a19a6d3c51e704846
Elijah <elijahoneal@yahoo.com>
2020-12-10 23:13:49 -0500

    Run successful name test

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -3,24 +3,20 @@ describe("These are sample tests", () => {
     it("Renders our page to the screen" , () => {
         cy.visit("http://localhost:3000");
     });
-    it("Makes a simple assertion", () => {
-        // Assertion
-       cy.get(".text-class")
-    })
 })
-// describe("Name renders to DOM", () => {
-//     beforeEach( () =>{
-//         cy.visit("http://localhost:3000");
-//     } )
-//     it("Can navigate to site", () =>{
-//         cy.url().should("include", "localhost");
-//     })
+describe("Name renders to DOM", () => {
+    beforeEach( () =>{
+        cy.visit("http://localhost:3000");
+    } )
+    it("Can navigate to site", () =>{
+        cy.url().should("include", "localhost");
+    })
 
-//     // it("Get name input and type name", () =>{
+    it("Get name input and type name", () =>{
        
         
-//     //     cy.get("[data-cy=name-input]")
-//     //     .should("have.value", "")
-//     //     .type("My name is chicka chicka slim slady");
-//     // })
-// })
+        cy.get("[data-cy=name-input]")
+        .should("have.value", "")
+        .type("My name is chicka chicka slim slady");
+    })
+})
