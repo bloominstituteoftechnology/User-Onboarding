3c613e889a4d364b81895f19e7fe05d54251b61a
Nathan Hietala <Nrhietala@gmail.com>
2020-12-16 14:59:05 -0600

    Working on painted new member to DOM

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Form from './components/Form'
 import React, { useState } from 'react';
+import axios from 'axios';
+import * as yup from 'yup';
 
 
 const initialFormValues = {
@@ -12,11 +14,42 @@ const initialFormValues = {
 
 function App() {
   const [ formValues, setFormValues ] = useState(initialFormValues)
+  const [ members, setMembers ] = useState("")
+
+  const onChange = event => {
+    const { name, value, type, checked } = event.target;
+
+    const valueToUse = type === 'checkbox' ? checked : value;
+    setFormValues({
+      ...formValues, [ name ] : valueToUse
+    })
+  }
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    const newMember = {
+      name:formValues.name.trim(),
+      email:formValues.email.trim(),
+      password:formValues.password.trim(),
+      terms:formValues.terms.trim(),
+    }
+    setMembers(newMember, ...members )
+  }
 
   return (
     <div className="App">
-      <h1>App Working!</h1>
-      <Form/>
+      <h1>User Onboarding Form</h1>
+      <Form
+      values={formValues}
+      onChange={onChange}
+      onSubmit={onSubmit}
+      />
+      {/* {
+        members.map(mem => {
+          return <div>{mem.name}</div>
+        })
+      } */}
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Form() {
+
+function Form(props) {
+  const { onChange, onSubmit, values } = props;
+
   return (
     <div>
-      <h1>Form Component</h1>
+      <form onSubmit={onSubmit}>
+        <label>Name
+          <input
+          type="text"
+          name="name"
+          onChange={onChange}
+          value={values.name}
+          />
+        </label>
+        <br/>
+        <label>Email
+          <input
+          type="email"
+          name="email"
+          onChange={onChange}
+          value={values.email}
+          />
+        </label>
+        <br/>
+        <label>Password
+          <input
+          type="password"
+          name="password"
+          onChange={onChange}
+          value={values.password}
+          />
+        </label>
+        <br/>
+        <label>Gimme all your data
+          <input
+          type="checkbox"
+          name="terms"
+          onChange={onChange}
+          checked={values.terms}
+          />
+        </label>
+        <br/>
+        <br/>
+        <button>Submit New Member</button>
+      </form>
     </div>
   )
 }
