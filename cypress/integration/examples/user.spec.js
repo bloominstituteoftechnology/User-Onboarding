e69ae3bda6ce2cc7285c50e6252f6880fe492a5c
Diego Roman <diegormnv209@gmail.com>
2020-12-17 12:52:54 -0800

    added test to type inputs and to submit form

--- cypress/integration/examples/user.spec.js
+++ cypress/integration/examples/user.spec.js
@@ -9,6 +9,7 @@ describe('User Onboarding App', () =>{
     const email = () => cy.get("input[name='email']")
     const password = () => cy.get("input[name='password']")
     const submitBtn = () => cy.get('button')
+    const terms = () => cy.get("input[name='termsOfService']")
 
 
     it('test to see if it works', () =>{
@@ -22,6 +23,36 @@ describe('User Onboarding App', () =>{
         email().should('exist');
         password().should('exist');
         submitBtn().should('exist');
+        terms().should('exist');
+    })
+
+    it('Can type into the inputs', () =>{
+        firstName()
+        .should('have.value', '')
+        .type('Diego')
+        .should('have.value', 'Diego')
+        lastName()
+        .should('have.value', '')
+        .type('Roman')
+        .should('have.value', 'Roman')
+        email()
+        .should('have.value', '')
+        .type('diegoroman@hotmail.com')
+        .should('have.value', 'diegoroman@hotmail.com')
+        password()
+        .should('have.value', '')
+        .type('diegoroman123')
+        .should('have.value', 'diegoroman123')
+        terms()
+        .check();
+    })
+    it('can submit the form data', () =>{
+        firstName().type('Diego')
+        lastName().type('Roman')
+        email().type('diegoroman@hotmail.com')
+        password().type('diegoroman123')
+        terms().check();
+        submitBtn().click();
     })
 
 
