c32e6dac524dca3882fb808c6ec011d11050d8b9
MaurooRen <maurooren@gmail.com>
2020-12-17 10:29:36 -0500

    setting states and form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
 
 function App() {
+
+  const initialFormValues = {
+    name: '',
+    email: '',
+    password: '',
+    terms: false,
+  }
+
+  const initialUsers = []
+
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+
+  const inputChange = (name, value) => {
+    console.log(name, value)
+  }
+
+
   return (
     <div className="container">
-      <Form />
+      <Form
+        values={formValues}
+        change={inputChange}
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,61 @@
 import React from 'react'
 
-const Form = () => {
+const Form = (props) => {
+
+    const { change, values } = props
+
+    const onChange = (event) => {
+        const { name, value } = event.target
+        change(name, value)
+    }
+
+    const onSubmit = () => {
+
+    }
+
     return (
-        <div>
-            <h1>I'm a nice form</h1>
-        </div>
+        <form onSubmit={onSubmit}>
+            <label htmlFor="name">Full Name:</label>
+            <input
+                id="name"
+                name="name"
+                type="text"
+                onChange={onChange}
+                value={values.name}
+            />
+
+            <label htmlFor="email">Email:</label>
+            <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={onChange}
+                value={values.email}
+            />
+
+            
+            <label htmlFor="password">Email:</label>
+            <input
+                id="password"
+                name="password"
+                type="text"
+                onChange={onChange}
+                value={values.password}
+            />
+
+            <label>
+                <input
+                    type="checkbox"
+                    name="terms"
+                    value={values.terms}
+                    checked={values.terms}
+                    onChange={onChange}
+                />
+                Terms of Service.
+            </label>
+
+            <button type="submit">Submit</button>
+        </form>
     )
 }
 
