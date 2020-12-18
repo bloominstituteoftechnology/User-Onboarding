28a6c058aa47ea3366db33dadecc61f3b6eb3f3c
Mark <mark@Marks-MacBook-Air.local>
2020-12-17 19:04:36 -0500

    quality of life improvements

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -18,68 +18,63 @@ const Form = () => {
     
     return (
         <>
-        <form>
-            <div className="names">
-                <input
-                    onChange={change}
-                    value={form.firstName}
-                    name="firstName"
-                    className="first"
-                    type="text"
-                    placeholder="First Name"
-                ></input>
-                <br />
+            <form>
+                <div className="names">
+                    <input
+                        onChange={change}
+                        value={form.firstName}
+                        name="firstName"
+                        className="first"
+                        type="text"
+                        placeholder="First Name"
+                    ></input>
+                    <input
+                        onchange={change}
+                        value={form.lastName}
+                        name="lastName"
+                        className="last"
+                        type="text"
+                        placeholder="Last Name"
+                    ></input>
                 </div>
-                <input
-                    onchange={change}
-                    value={form.lastName}
-                    name="lastName"
-                    className="last"
-                    type="text"
-                    placeholder="Last Name"
-                ></input>
-            
-            <br />
-            <div className="form">
-                <input 
-                    onChange={change}
-                    value={form.email}
-                    name="email"
-                    className="large"
-                    type="text"
-                    placeholder="Email"
-                ></input>
-                <br />
-                <input 
-                    onChange={change}
-                    value={form.password}
-                    name="password"
-                    className="large"
-                    type="text"
-                    placeholder="Password"
-                ></input>
-                <br />
-                <input 
-                    onChange={change}
-                    value={form.passwordConf}
-                    name="passwordConf"
-                    className="large"
-                    type="text"
-                    placeholder="Confirm Password"
-                ></input>
-                <br />
-                <label>
-                <input 
-                    onChange={change}
-                    value={form.checkbox}
-                    name="checkbox"
-                    className="checkbox"
-                    type="terms"
-                ></input>
-                Agree to Terms and Conditions
-                </label>
-            </div>
-        </form>
+                <div className="form">
+                    <input 
+                        onChange={change}
+                        value={form.email}
+                        name="email"
+                        className="large"
+                        type="text"
+                        placeholder="Email"
+                    ></input>
+                    <input 
+                        onChange={change}
+                        value={form.password}
+                        name="password"
+                        className="large"
+                        type="text"
+                        placeholder="Password"
+                    ></input>
+                    <br />
+                    <input 
+                        onChange={change}
+                        value={form.passwordConf}
+                        name="passwordConf"
+                        className="large"
+                        type="text"
+                        placeholder="Confirm Password"
+                    ></input>
+                    <label>
+                    <input 
+                        onChange={change}
+                        value={form.checkbox}
+                        name="checkbox"
+                        className="checkbox"
+                        type="terms"
+                    ></input>
+                    Agree to Terms and Conditions
+                    </label>
+                </div>
+            </form>       
         </>
     )
 }
