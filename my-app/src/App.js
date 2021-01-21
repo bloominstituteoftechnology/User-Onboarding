62e91b4609c7124c887b9820d58c353ee81ad43c
miketillman95 <matillman95@gmail.com>
2021-01-21 15:57:17 -0800

    hmwk

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, {useState} from 'react'
+import axios from 'axios'
+import * as yup from 'yup'
+import Form from './Form'
 
-initialFormValues ={
+const schema = yup.object().shape({
+  username: yup
+  .string()
+  .email('valid username')
+  .required('must include username'),
+  email: yup
+  .string()
+  .email('valid email address')
+  .required('must include email'),
+  password: yup
+  .string()
+  .email('valid password')
+  .required('must include password')
+  .min(6, 'password must be at least 6 characters long')
+
+})
+
+const initialFormValues ={
   username: '',
   email: '',
   password: '',
@@ -21,30 +41,31 @@ function App() {
 
   const submitForm = () =>{
 const newUser = {
-  username: formValues.username.trim(),
+      username: formValues.username.trim(),
       email: formValues.email.trim(),
       password: formValues.password,
 }
 
+
+    axios
+        .post(`https://reqres.in/api/users`, newUser)
+        .then((res) => {
+          console.log(res.data)
+          const getUser = res.data;
+        // set res.data as a user state and render it as HTML pre-tag
+
+
+
+        })
   }
 
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Onboarding</h1>
+      <Form update={setFormValues} values={formValues} submit={submitForm}/>
+
+      
     </div>
   );
 }
