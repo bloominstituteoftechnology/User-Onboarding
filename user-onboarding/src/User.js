c12921acb7d7f41763e52b23ddb0895a76b1a56a
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-01-27 16:57:04 -0800

    mvp completed

---
+++ user-onboarding/src/User.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function User({ details }) {
+  if (!details) {
+    return <h3>Working fetching your User&apos;s details...</h3>
+  }
+
+  return (
+    <div className='user container'>
+      <h2>{details.name}s</h2>
+      <p>Email: {details.email}</p>
+      <p>Password: Hidden</p>
+      
+    </div>
+  )
+}
+
+export default User
---
+++ user-onboarding/src/validation/formSchema.js
@@ -0,0 +1,22 @@
+import * as yup from "yup";
+
+
+export default yup.object().shape({
+    name: yup
+    .string()
+    .required("Name is required")
+    .min(4, "Name must be at least 4 characters long"),
+    email: yup
+    .string()
+    .email("must be a valid email")
+    .required("email is required"),
+    password: yup
+    .string()
+    .required("Password is required"),
+    terms: yup
+    .boolean()
+    .required("Need to agree to terms of service"),
+})
+
+
+
