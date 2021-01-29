80c8eb530de1791071fd31df72098bcb8595b053
Carson Myers <carsonscottmyers@gmail.com>
2021-01-28 22:11:17 -0700

    MVP2

---
+++ useronboarding/src/components/Friend.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Friend({ details }) {
+  if (!details) {
+    return <h3>Working fetching your friend&apos;s details...</h3>
+  }
+
+  return (
+    <div className='friend container'>
+      <h2>{details.first_name} {details.last_name}</h2>
+      <p>Email: {details.email}</p>
+
+    </div>
+  )
+}
+
+export default Friend
