96ac242522994cfbb2da891572f8c29d28e451f4
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2021-02-24 13:52:52 -0700

    Test

---
+++ form-wizard/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./Form";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
