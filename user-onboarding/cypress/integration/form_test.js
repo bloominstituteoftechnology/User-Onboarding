620371c289038f40d2d02665180bed4a61b77844
L-Steinmacher <dasai.sacul@gmail.com>
2021-02-25 15:07:10 -0800

    first commit and almost finished with individual tests

---
+++ user-onboarding/cypress/integration/form_test.js
@@ -0,0 +1,57 @@
+describe('user onboard app', ()=>{
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+    })
+
+    const userNameInput = () => cy.get('input[name=username]')
+    const emailInput = () => cy.get('input[name=email]')
+    const passwordInput = () => cy.get('input[name=password]')
+    const submitBtn = () => cy.get('#submit')
+    const termsOfService = () => cy.get('input[name=termsOfService]')
+    const roll = () => cy.get('select[name=roll]')
+    
+    it('sanity test', () => {
+        expect(1 + 1).to.equal(2)
+    })
+
+    describe('form test', () => {
+        it('submit btn is disabled when fields are empty', () => {
+            // just kicking the tires.
+            submitBtn().should('be.disabled')
+
+        })
+
+        it('Testing individual elements', () => {
+            userNameInput()
+              .should('have.value', '')
+              .type('IndyDog')
+              .should('have.value', 'IndyDog')
+            emailInput()
+              .should('have.value', '')
+              .type('dogzilla@dogmail.com')
+              .should('have.value', 'dogzilla@dogmail.com')
+            passwordInput()
+              .should('have.value', '')
+              .type('dOgPaSsWoRd')
+              .should('have.value', 'dOgPaSsWoRd')
+            termsOfService()
+              .click()
+              .should('have.value', 'agree')
+              .click()
+            roll()
+              .should('have.value', '')
+              .select('Wizard')
+              .should('.have.value', 'wizard')
+
+        })
+
+        it('Lets try to fill out the entire form and clicking the TOS button twice', () => {
+            //fill in name then password and email
+            //click TOS then select roll
+            //click the TOS a second time
+            //click the submit button to see if you can submit with the TOS unclicked
+
+        })
+    })
+
+})
