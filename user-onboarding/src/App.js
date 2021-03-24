b6b4fe24257faf28fb405d34529947f3f4803c8a
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-03-24 15:43:56 -0400

    Initialized project and completed task 1

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Form from './Form'
+
+function App() {
+  return (
+    <Form />
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Form(props) {
+    console.log('hi')
+    return (
+      <h2>Test</h2>
+    )
+}
