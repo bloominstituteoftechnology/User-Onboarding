230d36893241c252a55e0f0f3714660770c183f6
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-03-24 18:39:22 -0400

    Added onChange and onSubmit

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,30 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as yup from 'yup'
 
-export default function form(props) {
+export default function Form(props) {
+
+    const [forms, setForms] = useState({
+        username: '',
+        email: '',
+        password: '',
+        tos: false,
+    })
+
+    const change = evt => {
+      const { checked, value, name, type } = evt.target;
+      const valueToUse = type === 'checkbox' ? checked : value;
+      setForms({ ...forms, [name]: valueToUse})
+    }
+
+    const submit = evt => {
+      evt.preventDefault()
+      
+    }
+
+
     return (
       <div>
         <h2>Welcome, new user!</h2>
         <label>
           Name:
-          <input name='username' type='text'/>
+          <input name='username' type='text' value={forms.username} onChange={change}/>
         </label>
         <br></br>
         <label>
           Email:
-          <input name='email' type='text'/>
+          <input name='email' type='text' value={forms.email} onChange={change}/>
         </label>
         <br></br>
         <label>
           Password:
-          <input name='password' type='text'/>
+          <input name='password' type='text' value={forms.password} onChange={change}/>
         </label>
         <br></br>
         <label>
           Do you accept the terms of service?
-          <input name='tos' type='checkbox'/>
+          <input name='tos' type='checkbox' value={forms.tos} onChange={change}/>
         </label>
         <br></br>
-        <button>Join</button>
+        <button onSubmit={submit}>Join</button>
       </div>
       
     )
