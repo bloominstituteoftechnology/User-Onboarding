ebd59644c8cd79133013bf211925939896fc599d
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-03-25 09:47:36 -0400

    Restructured project

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,119 @@
+// Imports //
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Form from './Form'
+import schema from './schema';
+import Card from './Card';
+import Form from './Form';
+import axios from 'axios';
+import * as yup from 'yup';
+
+// Initialization //
+const initialFormValues = {
+  username: '',
+  email: '',
+  password: '',
+  tos: false,
+};
+
+const initialFormErrors = {
+  username: "",
+  email: "",
+  password: "",
+};
+
+const initialFriends = [];
+const initialDisabled = true;
 
 function App() {
+
+  // States //
+  const [friends, setFriends] = useState(initialFriends);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  // Helpers //
+  const getFriends = () => {
+    axios
+      .get(`https://reqres.in/api/users`)
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const postNewFriend = (newFriend) => {
+    axios
+      .post(`https://reqres.in/api/users`, newFriend)
+      .then(res => {
+        setFriends([res.data, ...friends]);
+        setFormValues(initialFormValues);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  // Event Handlers //
+  const inputChange = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(() => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
+      setFormValues({
+        ...formValues,
+        [name]: value,
+      });
+  }
+
+  const formSubmit = () => {
+    const newFriend = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      tos: formValues.tos
+    };
+    postNewFriend(newFriend);
+  };
+
+  // Side Effects //
+  useEffect(() => {
+    getFriends();
+  }, []);
+
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
+
+
+
   return (
-    <div>
-      <Form />
+    <div className='container'>
+      <Form 
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+      {friends.map((friend) => {
+        return <Card key={friend.id} details={friend} />;
+      })}
     </div>
     
   );
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,51 +1,54 @@
-import React, { useState } from 'react'
-import * as yup from 'yup'
+import React from 'react'
 
 export default function Form(props) {
+  const { values, submit, change, disabled, errors } = props;
+    
+  const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+  }
 
-    const [forms, setForms] = useState({
-        username: '',
-        email: '',
-        password: '',
-        tos: false,
-    })
+  const onChange = evt => {
+    const { checked, value, name, type } = evt.target;
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change(name, valueToUse);
+  }
 
-    const change = evt => {
-      const { checked, value, name, type } = evt.target;
-      const valueToUse = type === 'checkbox' ? checked : value;
-      setForms({ ...forms, [name]: valueToUse})
-    }
-
-    const submit = evt => {
-      evt.preventDefault()
-      
-    }
+    
 
 
     return (
       <div>
         <h2>Welcome, new user!</h2>
-        <label>
-          Name:
-          <input name='username' type='text' value={forms.username} onChange={change}/>
-        </label>
-        <br></br>
-        <label>
-          Email:
-          <input name='email' type='text' value={forms.email} onChange={change}/>
-        </label>
-        <br></br>
-        <label>
-          Password:
-          <input name='password' type='text' value={forms.password} onChange={change}/>
-        </label>
-        <br></br>
-        <label>
-          Do you accept the terms of service?
-          <input name='tos' type='checkbox' value={forms.tos} onChange={change}/>
-        </label>
-        <br></br>
-        <button onSubmit={submit}>Join</button>
+        <form className="form container" onSubmit={onSubmit}>
+          <label>
+            Name:
+            <input name='username' type='text' value={values.username} onChange={onChange}/>
+          </label>
+          <br></br>
+          <label>
+            Email:
+            <input name='email' type='text' value={values.email} onChange={onChange}/>
+          </label>
+          <br></br>
+          <label>
+            Password:
+            <input name='password' type='text' value={values.password} onChange={onChange}/>
+          </label>
+          <br></br>
+          <label>
+            Do you accept the terms of service?
+            <input name='tos' type='checkbox' value={values.tos} onChange={onChange}/>
+          </label>
+          <br></br>
+          <button disabled={disabled} >Join</button>
+          <div style={{ color: 'red' }}>
+            <div>{errors.username}</div>
+            <div>{errors.email}</div>
+            <div>{errors.password}</div>
+            <div>{errors.tos}</div>
+        </div>
+        </form>
       </div>
       
     )
