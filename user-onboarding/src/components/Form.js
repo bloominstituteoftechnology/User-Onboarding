8b2387fdf38ee9e6b18415b662030bfe81e99c93
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-21 18:31:50 -0400

    added inputs for name,email and password

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = (props) => {
-    
+//Managing state for form inputs
+const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    password: '',
+    terms_of_service: false
+
+})
+
+//onSubmit call-back function
+const formSubmit = event => {
+    event.preventDefault();
+    console.log('form submitted!');
+}
+
+//onChange call-back Function 
+const inputChange = event => {
+    setFormState({...formState, [event.target.name]: event.target.value})
+}
+
     return (
         <div>
-            <form>
+            <form onSubmit={formSubmit}>
                 <label>
                     Name: 
                     <input type='text' 
-                    placeholder='Name' 
+                    value={formState.name}
+                    onChange={inputChange} 
                     name='name' />
                 </label>
+                <label>
+                    Email: 
+                    <input type='email' 
+                    value={formState.email}
+                    onChange={inputChange} 
+                    name='email' />
+                </label>
+                <label>
+                    Password: 
+                    <input type='password' 
+                    value={formState.password}
+                    onChange={inputChange} 
+                    name='password' />
+                </label>
+                
+                <br />
+                <button>Submit</button>
             </form>
         </div>
     )
