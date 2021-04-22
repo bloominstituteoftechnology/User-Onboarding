ef3525bdd47a5e9c7e25f9380183e421735f507b
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-22 15:56:48 -0400

    set up tests to check that all elements that should exist actually exist

--- user-onboarding/cypress/integration/examples/form_test.js
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -4,7 +4,14 @@ describe('user onboarding', () => {
         cy.visit('http://localhost:3000/')
     })
 
-    it('name check', () => {
-        
+    it('proper elements exist', () => {
+        expect(5).to.equal(5)
+        expect(5).to.eql(5)
+        expect(5 + 1).to.not.equal(7)
+        cy.get('input[name=name]').should('exist')
+        cy.get('input[name=email]').should('exist')
+        cy.get('input[name=password]').should('exist')
+        cy.get('input[name=terms]').should('exist')
+        cy.get('button').should('exist')
     })
 })
