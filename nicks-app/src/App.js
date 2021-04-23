91fa1dc0d169f2796b4276761f39143668f0981b
nwraimo <raimo.nick9@gmail.com>
2021-04-22 23:39:47 -0700

    module 4

--- nicks-app/src/App.js
+++ nicks-app/src/App.js
@@ -6,6 +6,18 @@ import './App.css';
 //- [ ] Terms of Service (checkbox)
 //- [ ] A Submit button to send our form data to the server.
 
+// Task 1: Set up Project
+// This project is a continuation of the work you have done previously.
+
+//  CD into your old project
+//  Continue to make changes and push to the same branch
+//  Install Cypress with npm. npm install cypress --save-dev
+//  run npx cypress open, After a moment, the Cypress Test Runner will launch.
+//  Locate the integration folder at cypress/integration in your code editor
+//  Create a form_test.js file.
+//  Watch the Cypress Test Runner update the list of specs.
+//  Launch Cypress in interactive mode.
+
 function App() {
   return (
     <div className="App">
