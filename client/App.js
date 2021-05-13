af68c6187c0fb1e93282b96e5dcbc4ae6cb31850
JoshuaHolloway <jhollow6@asu.edu>
2021-05-12 20:47:11 -0500

    Shopify Storefront API

--- client/App.js
+++ client/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import Component from './Components/Component.js';
+import API_KEY from './env.js';
 
 const App = () => {
+
+  const query = `{
+                    shop {
+                      name
+                    }
+                  }`;
+
+  function apiCall(query) {
+    return fetch('https://thememinimal.myshopify.com/api/graphql.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/graphql',
+        'X-Shopify-Storefront-Access-Token': API_KEY
+      },
+      body: query
+    }).then(response => response.json());
+  }
+
+
+  apiCall(query).then(response => {
+
+    console.log('Store Front API GraphQL Response: ');
+    console.log(response);
+  });
+
   return (
     <div className="container">
       <Component />
