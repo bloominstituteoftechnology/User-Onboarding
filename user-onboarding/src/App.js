69783c3857547aa11082e617d11be71ab01e3281
John Vehmeyer <j.vehmeyer@gmail.com>
2021-05-19 09:20:48 -0500

    created scaffolding for user onboarding form

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import './App.css';
+import Form from './Form';
+
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          User Onboarding App
+        </p>
+      
+       </header>
+    </div>
+  );
+}
+
+export default App;
