d82f4f5e96457f4295c79775c0fc2860a8df9cd5
John Vehmeyer <j.vehmeyer@gmail.com>
2021-05-20 16:55:56 -0500

    wip

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,6 +1,6 @@
 describe('User OnBoarding Form App', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:3000/')
+        cy.visit('http://localhost:3000/');
     })
 
 
@@ -11,10 +11,43 @@ describe('User OnBoarding Form App', () => {
         expect({}).to.eql({})
     })
 
-    const submitBtn = () => cy.get('button[id=submitBtn]')
+    const submitBtn = () => cy.get('button[id="submitBtn"]')
+    const nameInput = () => cy.get('input[name="name"]')
+    const emailInput = () => cy.get('input[name="email"]')
+    const passwordInput = () => cy.get('input[name="password"]')
+    const termsCheckBox = () => cy.get('input[name="termsOfService"]')
 
     it('proper elements are showing', () => {
         submitBtn().should('exist')
+    })
+
+    it('can type in name input', () => {
+        nameInput()
+            .should("have.value", "")
+            .type("John")
+            .should("have.value", "John")
+    })
+
+    it('can type in email input', () => {
+        emailInput()
+            .should("have.value", "")
+            .type("john@aol.com")
+            .should("have.value", "john@aol.com")
+    })
+
+    it('can type in password input', () => {
+        passwordInput()
+            .should("have.value", "")
+            .type("abcpass")
+            .should("have.value", "abcpass")
+    })
+
+    it('can check terms of service check box', () => {
+        termsCheckBox()
+            .check("value", "false")
+            .click()
+            .should("value", "true")
+
     })
 
 })
