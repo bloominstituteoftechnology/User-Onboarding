12958aa330e2c6f62146f9fb34d5db254d34a8a5
John Vehmeyer <j.vehmeyer@gmail.com>
2021-05-20 14:32:41 -0500

    added form_test.js for testing with Cypress

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,13 @@
+describe('User OnBoarding Form App', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:59952')
+    })
+
+
+    it('sanity check to make sure tests work', () => {
+        expect(1+2).to.equal(3)
+        expect(2+2).not.to.equal(5)
+        expect({}).not.to.equal({})
+        expect({}).to.eql({})
+    })
+})
