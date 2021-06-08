9fc801a070748ff1a35f006eac58263164dbb739
Sarah D Herren <herren.sarah@icloud.com>
2021-06-08 10:55:24 -0500

    created react app, userForm component

---
+++ project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UserForm from './components/UserForm';
+
+function App() {
+  return (
+    <div className="App">
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ project/src/components/UserForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+const UserForm = () => {
+
+    return(
+        <div>
+            <h2>Add New User:</h2>
+            <Form>
+
+            </Form>
+        </div>
+    )
+}
+
+export default UserForm;
