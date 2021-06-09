a43dd7699632ae5da7f9a92bc3b7c337ab3313c0
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-06-08 21:29:29 -0700

    added css to the warning prompts

--- src/User.js
+++ src/User.js
@@ -9,7 +9,7 @@ return (
         <h2>{details.username}</h2>
         <p>{details.password}</p>
         <p>Email: {details.email}</p>
-        <p>Agree to Terms: {details.termsOfService}</p>
+        <p>Agree to Terms: Yes {details.termsOfService}</p>
     </div>
 )
 
