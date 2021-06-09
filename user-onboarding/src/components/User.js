cbff2306246669e05d602635caaaee769067d18c
Greg Brewton <gbrewton1980@gmail.com>
2021-06-08 21:34:49 -0600

    Adds cursor: pointer to tosCheck box

--- user-onboarding/src/components/User.js
+++ user-onboarding/src/components/User.js
@@ -2,9 +2,21 @@
 const User = (props) => {
   const { name, email } = props;
   return (
-    <div className="user">
-      <h2>{name}</h2>
-      <a href="#">{email}</a>
+    <div class="row">
+      <div class="col s12 m6">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">{name}</span>
+            <p>
+              email: {email}
+            </p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
