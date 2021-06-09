c8a42d8cfb8b6fac6e4fda1e552c0cc6caeceecd
Aaron <aarongabriel147@gmail.com>
2021-06-08 20:05:04 -0700

    All complete except for the very last section

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Form from "./components/Form";
+
+
+
+
+function App() {
+
+  // const [] = useState
+
+  return (
+
+    <div className="App">
+      <p>User Onboarding Project</p>
+      <Form />
+    </div>
+
+  );
+}
+
+export default App;
