ad4176c3236255361ea2d8ca0584e374a636e81c
Amlak befekadu <amlakbarkon@gmail.com>
2021-06-15 13:38:36 -0400

    validation start

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -5,36 +5,44 @@ function Form (){
        email:'',
        password: '',
        agree: false,
-       language:''
+       language:'',
+       star:""
+      
    })
-
+   const [ disabled, setDisabled ] = useState(true)
+ const change = event=>{
+     const {name, value, checked, type} = event.target;
+     const valueToUse = type === 'checkbox' ? checked : value;
+     setForm({...form, [name]: valueToUse})
+    
+ }
     return (
         <div className="App">
             <label htmlFor="firstName">First Name:
-            <input type="text" value={form.name} name="user" placeholder="First Name" />
+            <input onChange={change} type="text" value={form.name} name="user" placeholder="First Name" />
             </label>< br/>
             <label htmlFor="email">Email:
-            <input type="email" value={form.email} name="email" placeholder="Email" />
+            <input onChange={change} type="email" value={form.email} name="email" placeholder="Email" />
             </label>< br/>
             <label htmlFor="password">PassWord:
-            <input type="password"  value={form.password} name="password" placeholder="Password" />
+            <input onChange={change} type="password"  value={form.password} name="password" placeholder="Password" />
             </label>< br/>
             <label > Terms of Service
-                <input checked={form.agree} name="agree" type="radio" />
+                <input onChange={change} checked={form.agree} name="agree" type="radio" />
             </label>< br/>
             <label>Star Trek:
-                <input  checked={form.star === "trek"} value="trek"name="star" type="radio" />
+                <input onChange={change}  checked={form.star === "trek"} value="trek"name="star" type="radio" />
             </label>
             <label>Star Wars:
-                <input  checked={form.wars ==='star'} value="wars" name="star" type="radio" />
+                <input  onChange={change} checked={form.star ==='wars'} value="wars" name="star" type="radio" />
             </label><br/>
-            <select value={form.language} name="language">
+            <select onChange={change} value={form.language} name="language">
                 <option value="">Select Language</option>
                 <option value="1">JavaScript</option>
                 <option value="2">Python</option>
                 <option value="3">Node</option>
             </select>< br/>
-            <button type="submit">Submit</button>
+            <button type="submit" disabled={disabled}>Submit</button>
         </div>
     )
 }
