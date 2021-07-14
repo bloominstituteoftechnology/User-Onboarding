4b4079a8f215d6b25becd3d791a83147cfcad4a3
Dalian <DalianAlyxander@me.com>
2021-07-14 02:42:20 -0400

    Completed Form component

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MMO Discussion Board</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
