861935b30239b6d652dccb319d1dc5a5bd8c180a
Mykael Barnes <64759782+Mykeb96@users.noreply.github.com>
2021-07-13 18:24:32 -0700

    started project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Form from 'components/Form.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,8 @@
+
+
+export default function Form(props){
+
+    return (
+        <div>Testing</div>
+    )
+}
