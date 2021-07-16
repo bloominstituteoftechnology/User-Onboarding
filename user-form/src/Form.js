6081b42f60b84ba234df833b5dbf0c33c542fa0e
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-07-16 12:38:37 -0500

    added usestate to handle inputchange and added a formsubmit logic to preventdefault function of submit button

--- user-form/src/Form.js
+++ user-form/src/Form.js
@@ -9,38 +9,60 @@ function Form() {
 		password: "",
 		terms:""
 	})
+
+	//this controls the inputchange based on the type of input
+	const inputChange = event => {
+		//newformdata takes in what we type into input
+		//then makes a copy of formstate and makes a new state. 
+		//then we make setformstate into that new state we made and tada! 
+		const newFormData = {
+			...formState,
+			[event.target.name]:
+				event.target.type === "checkbox" ? event.target.checked : event.target.value
+		}
+		setFormState(newFormData)
+	}
+
+	const formSubmit = (e) => {
+		e.preventDefault()
+	}
+
 	return (
-		<form>
+		<form onSubmit={formSubmit}>
 			<label htmlFor="name">Name:
-				<input
+				<input 
+					onChange={inputChange}
 					type="text"
 					name="name"
 					id="name"
-					value="" />
+					value={formState.name} />
 			</label>
 
 			<label htmlFor="email">Email
-				<input
+				<input 
+					onChange={inputChange}
 					type="email"
 					name="email"
 					id="email"
-					value="" />
+					value={formState.email} />
 			</label>
 
 			<label htmlFor="password">Password
-				<input
+				<input 
+					onChange={inputChange}
 					type="password"
 					name="password"
 					id="password"
-					value="" />
+					value={formState.password} />
 			</label>
 
 			<label htmlFor="terms" className="terms">
-				<input
+				<input 
+					onChange={inputChange}
 					type="checkbox"
 					name="terms"
 					id="terms"
-					checked="" />
+					checked={formState.terms} />
 					Terms of Service
 			</label>
 			<button type="submit">Submit</button>
