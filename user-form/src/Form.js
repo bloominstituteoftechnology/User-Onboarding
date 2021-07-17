da6a01d0fe1f9d104bbbbe9821af9e1e36f1d6ec
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-07-16 21:46:20 -0500

    project done

--- user-form/src/Form.js
+++ user-form/src/Form.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import * as yup from 'yup'
+import axios from 'axios'
 
 function Form() {
 
@@ -27,7 +28,7 @@ function Form() {
 		email: yup.string().email("Please enter a valid Email").required(),
 		password: yup.string().required(),
 		passwordConfirm: yup.string().oneOf([yup.ref("password"), null],"Passwords must match"),
-		terms:yup.bool().oneOf([true],"Please Agree to Terms and Conditions").required()
+		terms:yup.boolean().oneOf([true], 'Please Agree to the Terms & Conditions').required()
 	})
 
 	useEffect(() => {
@@ -43,7 +44,10 @@ function Form() {
 			.reach(formSchema, event.target.name)
 			.validate(event.target.value)
 			.then((valid) => {
-				setErrors({...errors, [event.target.name]: ""})
+				setErrors({
+					...errors,
+					[event.target.name]: ""
+				})
 			})
 			.catch((err) => {
 				setErrors({...errors, [event.target.name]: err.errors[0]})
@@ -66,6 +70,19 @@ function Form() {
 
 	const formSubmit = (e) => {
 		e.preventDefault()
+		axios
+			.post("https://reqres.in/api/users", formState)
+			.then((res) => {
+				console.log(res)
+				setFormState({
+					name: "",
+					email: "",
+					password: "",
+					passwordConfirm:"",
+					terms:""
+				})
+		})
+		
 	}
 
 
@@ -102,14 +119,14 @@ function Form() {
 			{errors.password.length > 0 ? <p className="error">{errors.password}</p> : null}
 			</label>
 
-			<label htmlfor="passwordConfirm">Confirm Password
+			<label htmlFor="passwordConfirm">Confirm Password
 				<input
 					onChange={inputChange}
 					type="password"
 					name="passwordConfirm"
-					id="password"
+					id="passwordConfirm"
 					value={formState.passwordConfirm} />
-				{formState.passwordConfirm.match(formState.password) ? null : errors.passwordConfirm}
+				{formState.passwordConfirm.match(formState.password) ? null : <p className="error">{errors.passwordConfirm}</p>}
 			</label>
 
 			<label htmlFor="terms" className="terms">
@@ -119,7 +136,7 @@ function Form() {
 					name="terms"
 					id="terms"
 					checked={formState.terms} />
-					Terms of Service
+				Terms of Service
 			</label>
 			<button disabled={disableButton} type="submit">Enroll</button>
 			
