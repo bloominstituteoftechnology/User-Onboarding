ca6e8c0c8888fecbb69b7da4940283c97c66a2f3
Klove Adams <kalebsxott@gmail.com>
2021-07-21 19:46:23 -0400

    mvp done

--- user-onboarding-app/src/App.js
+++ user-onboarding-app/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Form from "./Form";
 import schema from "./formSchema";
-import logo from './logo.svg';
-import './App.css';
+import { reach } from "yup";
+import axios from "axios";
 
 const initialFormValues = {
   username: "",
@@ -26,6 +26,63 @@ function App() {
   const [disabled, setDisabled] = useState(initialDisabled);
 
   // setUsers([newUser, ...users]);
+  const getUsers = () => {
+    axios.get("https://reqres.in/api/users")
+      .then(res => {
+        console.log(res.data)
+        setUsers(res.data.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  };
+
+  const postNewUser = newUser => {
+    axios.post("https://reqres.in/api/users", newUser)
+      .then(res => {
+        setUsers([res.data, ...users])
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
+  };
+
+  const validate = (name, value) => {
+    reach(schema, name)
+    .validate(value)
+    .then(() =>setFormErrors({ ...formErrors, [name]: '' }
+    ))
+    .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  };
+
+  const inputChange = (name, value) => {
+    validate(name, value)
+    setFormValues({
+      ...formValues,
+      [name]: value 
+    })
+  };
+
+  const formSubmit = () => {
+    const newUser = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password,
+      terms: false,
+    }
+    postNewUser(newUser)
+  };
+
+  useEffect(() => {
+    getUsers()
+  }, []);
+
+  useEffect(() => {
+    schema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
 
   return (
     <div className="App">
@@ -36,6 +93,16 @@ function App() {
         disabled={disabled}
         errors={formErrors}
       />
+
+      {users.map(user => {
+
+        return (
+          <div>
+            <h4>{user.username}</h4>
+            <p>{user.email}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
