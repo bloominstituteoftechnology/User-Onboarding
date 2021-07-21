546697e714271819d2b65bc236de50918ec81dc5
Rick <rmansfield@realpeoplesratings.com>
2021-07-21 10:29:59 -0500

    U2.3 M3 Progress push App.js & Form.js

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,68 +1,123 @@
-
-import './App.css';
-import React, { useState, useEffect } from 'react'
-import Form from './components/Form'
-import Users from './components/Users'
-import axios from 'axios';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
+import User from "./components/User";
+import axios from "axios";
+import * as yup from "yup";
+import schema from './validation/formSchema'
 
 const initialFormValues = {
-  username: '',
-  email: '',
-  password: '',
-  terms: '',
-}
+  username: "",
+  email: "",
+  password: "",
+  terms: "",
+};
 
 const initialFormErrors = {
-  username: '',
-  email: '',
-  password: '',
-  terms: '',
-}
+  username: "",
+  email: "",
+  password: "",
+  terms: "",
+};
 
-const initialUsers = []
-const initialDisabled = true
+const initialUsers = [];
+const initialDisabled = true;
 
 export default function App() {
-  const [users, setUsers] = useState(initialUsers) //array of users objects
-  const [formValues, setFormValues] = useState(initialFormValues) // object
-  const [formErrors, setFormErrors] = useState(initialFormErrors) // object
-  const [disabled, setDisabled] = useState(initialDisabled)       // boolean
+  const [users, setUsers] = useState(initialUsers); //array of users objects
+  const [formValues, setFormValues] = useState(initialFormValues); // object
+  const [formErrors, setFormErrors] = useState(initialFormErrors); // object
+  const [disabled, setDisabled] = useState(initialDisabled); // boolean
 
   const getUsers = () => {
-    axios.get('https://reqres.in/api/users')
-    .then(res => {
-      setUsers(res.data)
-      console.log(`HERE IS setUsers`, setUsers)
+    axios
+      .get("https://reqres.in/api/users")
+      .then((res) => {
+        setUsers(res.data);
+        console.log(`HERE IS setUsers`, setUsers);
+      })
+      .catch((err) => {
+        debugger;
+        console.log(err);
+      });
+  };
+
+  const postNewUser = (newUser) => {
+    axios
+      .post("https://reqres.in/api/users", newUser)
+      .then((res) => {
+        setUsers([...users, res.data]);
+        setFormValues(initialFormValues);
+      })
+      .catch((err) => {
+        debugger;
+        console.log(err);
+      })
+      .finally(() => {});
+  };
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
+  };
+
+  const inputChange = (name, value) => {
+    validate(name, value)
+    setFormValues({
+      ...formValues,
+      [name]: value // NOT AN ARRAY
     })
-    .catch(err => {
-      debugger 
-      console.log(err);
-    });
   }
 
-  const postNewUser = newUser => {
-    axios.post('https://reqres.in/api/users', newUser)
-    .then(res => {
-      setUsers([...users, res.data])
-      setFormValues(initialFormValues)
-    })
-    .catch(err => {
-      debugger
-      console.log(err);
-    })
-    .finally(() =>{})
+  const formSubmit = () =>{
+    const newUser = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      // 🔥 terms
+    }
+    postNewUser(newUser)
   }
-  return (
-  <div className="App"> 
-  <header><h1>User Onboarding</h1></header>
-            {/* <Form
-        values={formValues} 
-        update={updateForm} 
-        submit={submitForm}
-      /> */}
-    </div>
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  useEffect(() => {
     
+  }, [])
+
+  // useEffect(() => {
+  //   schema.isValid(formValues)
+  //   .then(valid => {
+  //     setDisabled(!valid);
+  //   });
+  // }, [formValues]);
+
+
+  return (
+    <div className="App">
+      <header>
+        <h1>User Onboarding</h1>
+      </header>
+      <Form
+        // values={formValues} 
+        // update={updateForm} 
+        // submit={submitForm}
+      />
+    </div>
   );
 }
-
-
--- onboarding/src/components/Form.js
+++ onboarding/src/components/Form.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function OnboardingForm(props) {
-const {
-    values, 
-    submit,
-    change,
-    disabled,
-    errors,
-} = props
-
-const onChange = evt => {
-    evt.preventDefault()
-    submit()
-}
+  const { values, submit, change, disabled, errors } = props;
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+  const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
+  };
+  return (
+    <form className="form container" onSubmit={onSubmit}>
+      <div>
+        <h2>Add a User</h2>
+      </div>
+    </form>
+  );
 }
