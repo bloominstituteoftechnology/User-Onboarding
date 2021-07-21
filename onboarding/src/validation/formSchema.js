2aea91a951fb4e3a80137fac4f0e72ccf39565df
Rick <rmansfield@realpeoplesratings.com>
2021-07-21 12:35:21 -0500

    Working on formSchema.js

---
+++ onboarding/src/validation/formSchema.js
@@ -0,0 +1,21 @@
+import * as yup from 'yup'
+
+export default yup.object().shape({
+    username: yup
+        .string()
+        .required("Username is requires")
+        .min(3, 'Username must be 3 chars or longer'),
+    email: yup
+        .string()
+        .email('Must be a valid email')
+        .required('Email is required'),
+    password: yup
+        .string()
+        .min(8).max(16)
+        .required('Must provide password')
+
+    //checkboxes are complete
+    // hiking: yup.boolean(),
+    // reading: yup.boolean(),
+    // coding: yup.boolean(),
+})
