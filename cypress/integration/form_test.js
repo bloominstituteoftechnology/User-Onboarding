9110427311fbab7afe91c986eb47ec30a6241ebb
Rick <rmansfield@realpeoplesratings.com>
2021-07-22 14:29:35 -0500

    U2.3 M4 Complete MVP cypress testing

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -8,19 +8,57 @@ describe('Onboarding App', () => {
         //assertion(s)
         expect(5).to.equal(5);
         expect(1 + 2).to.equal(3);
-    
         expect({}).to.not.equal({}); // can use "equal" here// and not strict i.e. (==)
         expect({}).to.eql({}); //can't use equal here// and deeply strick i.e. (===)
       });
 
       const usernameInput = () => cy.get('input[name="username"]');
-      it('Gets the name and adds text', () => {
+      const emailInput = () => cy.get('input[name="email"]');
+      const newEmail = "RicksMyCodeGuy@gmail.com";
+      const passwordInput = () => cy.get('input[name="password"]');
+      const newPassword = "123456789";
+      const checkBox = () => cy.get('input[type="checkbox"]');
+      const submitBtn = () => cy.get('button');
+
+      it('Checks form Fields, Submit, and Validation', () => {
+        submitBtn().should("exist");
+        submitBtn().should("be.disabled")
         usernameInput()
         .should("exist")
         .should("have.value", "")
         .type("Rick Mansfield")
         .should("have.value", "Rick Mansfield");
+        submitBtn().should("be.disabled")
+
+        emailInput()
+        .should("exist")
+        .should("have.value", "")
+        .type(newEmail)
+        .should("have.value", newEmail);
+        submitBtn().should("be.disabled")
         
+        passwordInput()
+        .should('exist')
+        .should('have.value', "")
+        .type(newPassword)
+        .should('have.value', newPassword)
+        submitBtn().should("be.disabled")
+
+        checkBox()
+        .check()
+        submitBtn().should("not.be.disabled");
+        submitBtn().click();
       });
+
+      // const emailInput = () => cy.get('input[name="email"]')
+      // const newEmail = "RicksMyCodeGuy@email.com"
+      // it('Gets Email and adds content', () => {
+      //   emailInput()
+      //   .should("exist")
+      //   .should("have.value", "")
+      //   .type(newEmail)
+      //   .should("have.value", newEmail);
+      // });
+      
       
 });
