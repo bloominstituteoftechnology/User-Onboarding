33c9f55b239c9bc0014f1538aaa1ae5e493acf5e
Klove Adams <kalebsxott@gmail.com>
2021-07-22 17:07:43 -0400

    almost done forgot to commit

--- user-onboarding-app/cypress/integration/form_test.js
+++ user-onboarding-app/cypress/integration/form_test.js
@@ -8,7 +8,7 @@ describe("User Onboarding app",()=>{
 
     beforeEach(()=>{
         cy.visit("http://localhost:3000")
-    })
+    });
 
     it("check if the right elements are showing",() => {
         userInput().should('exist')
@@ -16,6 +16,42 @@ describe("User Onboarding app",()=>{
         passwordInput().should('exist')
         termsInput().should('exist')
         submitBtn().should('exist')
+    });
+
+    it("get the user input and type a name in it",() => {
+        userInput().pause()
+          .should("have.value","")
+          .type("Greg")
+          .should("have.value","Greg")
+    });
+
+    it("get the email input and type a address in it",() => {
+        emailInput()
+          .should("have.value","")
+          .type("greg@reqres.in")
+          .should("have.value","greg@reqres.in")
+    });
+
+    it("get the password input and type a password in it",() => {
+        passwordInput()
+          .should("have.value","")
+          .type("gregspassword")
+          .should("have.value","gregspassword")
+    });
+
+    it("check to see if a user can check the terms",() => {
+        termsInput().click()
+    });
+
+    it("Check to see if a user can submit the form data",() => {
+        userInput().type("Greg")
+        emailInput().type("greg@reqres.in")
+        passwordInput().type("gregspassword")
+        termsInput().click().pause()
+        submitBtn().click()
+        userInput().should("have.value","")
+        emailInput().should("have.value","")
+        passwordInput().should("have.value","")
     })
 
 })
