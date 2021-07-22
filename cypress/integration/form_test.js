baba19e69bccddd9645173a6ce845e67f4525665
Rick <rmansfield@realpeoplesratings.com>
2021-07-22 12:47:34 -0500

    Cypress loaded in both inner and outter file WTF

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,15 @@
+describe('Onboarding App', () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+      });
+    
+    
+    it("sanity check", () => {
+        //assertion(s)
+        expect(5).to.equal(5);
+        expect(1 + 2).to.equal(3);
+    
+        expect({}).to.not.equal({}); // can use "equal" here// and not strict i.e. (==)
+        expect({}).to.eql({}); //can't use equal here// and deeply strick i.e. (===)
+      });
+});
