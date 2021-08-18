2a2929f76a584ddb6bf795641025dfe67862331e
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-08-18 12:02:57 -0600

    Initial project set up. Added Form.js. Added initial form values. Set initial state.

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './App.css';
+
+//components
+import Form from './components/Form';
+
+//initial form values
+const initialFormValues = {
+  //text input
+  name: '',
+  email: '',
+  password: '',
+  //dropdown
+  role: '',
+  //checkbox
+  termsOfService: false
+}
+
+const initialErrors = {
+  name: '',
+  email: '',
+  password: '',
+  role: '',
+  termsOfService: false
+}
+
+const initialTeamMembers = [];
+const initialDisabled = true;
+
+
+function App() {
+//state
+const [formValues, setFormValues] = useState(initialFormValues);
+const [teamMembers, setTeamMembers] = useState(initialTeamMembers);
+const [formErrors, setFormErrors] = useState(initialErrors);
+const [disabled, setDisabled] = useState(initialDisabled);
+
+//helper functions
+
+
+
+  return (
+    <div className="App">
+      <h1>Team Onboarding</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Form = (props) => {
+    
+    return (
+        <div>
+            <h3>This is a Form! </h3>
+            <form>
+                <label>
+                    <input
+                        type="text"
+                        name="name"
+                        
+                    />
+                </label>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
