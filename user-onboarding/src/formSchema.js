54a6a0848c203971d7070d124ca51ffb74d75491
Richard Oh <n24512@protonmail.com>
2021-08-18 18:19:13 -0500

    formSchema complete

---
+++ user-onboarding/src/formSchema.js
@@ -0,0 +1,24 @@
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+    name: yup
+        .string()
+        .trim()
+        .required('Name is required')
+        .min(2, 'Name must be at least 2 characters long'),
+    email: yup
+        .string()
+        .trim()
+        .email('Must be a valid email address')
+        .required('Email is required'),
+    password: yup
+        .string()
+        .trim()
+        .required('Password is required')
+        .min(8, 'Password must be at least 8 characters long'),
+    terms: yup
+        .string()
+        .oneOf(['yes', 'no'], 'Selection is required')
+})
+
+export default formSchema
