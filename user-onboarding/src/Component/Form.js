fb8b23c1ab4f47cffd515d05be6b724abc74584e
BrendonHawkins <artbitcodedata@gmail.com>
2021-08-19 09:42:47 -0400

    More added

---
+++ user-onboarding/src/Component/Form.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+export default function FriendForm(props) {
+  const {
+    values,
+    submit,
+    change,
+    disabled,
+    errors,
+  } = props
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+  }
+
+  const onChange = evt => {
+    /* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
+    const { name, value, checked, type } = evt.target
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change(name, valueToUse);
+  }
+
+  return (
+    <form className='form container' onSubmit={onSubmit}>
+      <div className='form-group submit'>
+        <h2>Add a User</h2>
+
+        <div className='errors'>
+          {/* 🔥 RENDER THE VALIDATION ERRORS HERE */}
+          <div>{errors.username}</div>
+          <div>{errors.email}</div>
+          <div>{errors.role}</div>
+          <div>{errors.civil}</div>
+        </div>
+      </div>
+
+      <div className='form-group inputs'>
+        <h4>General information</h4>
+
+        {/* ////////// TEXT INPUTS ////////// */}
+        {/* ////////// TEXT INPUTS ////////// */}
+        {/* ////////// TEXT INPUTS ////////// */}
+        <label>Username&nbsp;
+          <input
+            value={values.username}
+            onChange={onChange}
+            name='username'
+            type='text'
+          />
+        </label>
+
+        <label>Email
+          <input
+            value={values.email}
+            onChange={onChange}
+            name='email'
+            type='text'
+          />
+        </label>
+
+        <label>Password
+          <input
+            value={values.password}
+            onChange={onChange}
+            name='password'
+            type='text'
+          />
+        </label>
+        {/* ////////// CHECKBOXES ////////// */}
+        {/* ////////// CHECKBOXES ////////// */}
+        {/* ////////// CHECKBOXES ////////// */}
+        <label>Terms of Service
+          <input
+            type="checkbox"
+            name="terms"
+            checked={values.terms}
+            onChange={onChange}
+          />
+        </label>
+        
+        {/* 🔥 DISABLE THE BUTTON */}
+        <button disabled={disabled}>submit</button>
+      </div>
+    </form>
+  )
+}
