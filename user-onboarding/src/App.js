10943ddad19a739bd1076f31fd125141af6f74a4
Richard Oh <n24512@protonmail.com>
2021-08-18 19:12:30 -0500

    App.js update

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import axios from 'axios';
 import * as yup from 'yup';
 import Form from './Form';
+import formSchema from './formSchema';
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  terms: false,
+}
+
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  terms: '',
+}
+
+const initialUser = []
+const initialDisabled = true
+
 
 function App() {
+  const [users, setUsers] = useState(initialUser)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const getUsers = () => {
+    axios.get('https://buddies.com/api/friends')
+      .then(res => {
+        setUsers(res.data)
+      }).catch(err => console.error(err))
+  }
+
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(res => {
+        console.log(res)
+        setUsers([res.data, ...users])
+      }).catch(err => console.error(err))
+
+      setFormValues(initialFormValues)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
