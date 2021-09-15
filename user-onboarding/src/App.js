b1240941f1e603527e4726d550a26cdc68ef814d
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-09-15 12:33:47 -0700

    had some chicken for lunch. form nearly complete now. created text/checkbox/radio inputs for now.

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import Form from './components/Form';
+import axios from 'axios';
+import * as yup from 'yup';
+
+const initialValues = {
+  username: '',
+  email: '',
+  password: '',
+  terms: false
+}
+
 
 function App() {
   const [values, setValues] = useState([]);
 
+
   return (
     <div className="App">
-      <Form values={values}/>
+      <Form values={values} />
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -11,11 +11,58 @@ export default function Form(props) {
     return (
         <div>
             <form>
-                <label>
+{/* TEXTBOX TEXTBOX TEXTBOX TEXTBOX TEXTBOX TEXTBOX TEXTBOX TEXTBOX TEXTBOX TEXTBOX TEXTBOX  */}
+                <label>Username&nbsp;
                     <input 
                         type="text"
                         name="username"
                         value={values.username}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Email&nbsp;
+                    <input 
+                        type="text"
+                        name="email"
+                        value={values.email}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Password&nbsp;
+                    <input 
+                        type="text"
+                        name="password"
+                        value={values.password}
+                        onChange={onChange}
+                    />
+                </label>
+{/* RADIO RADIO RADIO RADIO RADIO RADIO RADIO RADIO RADIO RADIO RADIO RADIO RADIO RADIO RADIO  */}
+                <div>Preference
+                    <label>Part Time&nbsp;
+                        <input 
+                            type="radio"
+                            name="wrkPref"
+                            value="partTime"
+                            checked={values.wrkPref === "partTime"}
+                            onChange={onChange}
+                        />
+                    </label>
+                    <label>Full Time&nbsp;
+                        <input 
+                            type="radio"
+                            name="wrkPref"
+                            value="fullTime"
+                            checked={values.wrkPref === "fullTime"}
+                            onChange={onChange}
+                        />
+                    </label>
+                </div>
+{/*CHECKBOX CHECKBOX CHECKBOX CHECKBOX CHECKBOX CHECKBOX CHECKBOX CHECKBOX CHECKBOX CHECKBOX */}
+                <label>Terms&nbsp;
+                    <input 
+                        type="checkbox"
+                        name="terms"
+                        checked={values.terms}
                         onChange={onChange}
                     />
                 </label>
