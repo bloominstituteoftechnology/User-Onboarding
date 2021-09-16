65ba7f47ba1a8f324d49d5ba3a24e7a01e2f9180
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-09-16 15:19:53 -0700

    cypress test analyzes script correctly. everythings passing.

--- user-onboarding/cypress/integration/1-getting-started/form_test.js
+++ user-onboarding/cypress/integration/1-getting-started/form_test.js
@@ -6,9 +6,11 @@ describe('User-Onboarding App', () => {
     const usernameInput = () => cy.get('input[name=username]');
     const emailInput = () => cy.get('input[name=email]');
     const passwordInput = () => cy.get('input[name=password]');
-    const wrkPref = () => cy.get('input[name=wrkPref]');
+    const wrkPref = () => cy.get('input[type="radio"]');
     const termsInput = () => cy.get('input[name=terms]');
-    const submitBtn = () => cy.get(`button[id='submit']`)
+    const submitBtn = () => cy.get(`button[id='submit']`);
+
+    const workerElem = () => cy.get('[class="Worker"]');
 
     it('inputs exist', () => {
         usernameInput().should('exist');
@@ -47,8 +49,58 @@ describe('User-Onboarding App', () => {
 
         it('radio fields can be selected', () => {
             wrkPref()
+                .check("fullTime")
+                .should('be.checked');
+
+            wrkPref()
+                .check("partTime")
+                .should('be.checked');
+        })
+
+        it('checkbox field can be checked', () => {
+            termsInput()
+                .check()
+                .should('be.checked')
+        })
+    })
+
+    describe('all inputs can be filled', () => {
+
+        it('submit button is disabled', () => {
+            submitBtn().should('be.disabled');
+        })
+
+        it('input all fields', () => {
+            usernameInput()
+                .should('have.value', '')
+                .type('moselyBigg')
+                .should('have.value', 'moselyBigg')
+
+            emailInput()
+                .should('have.value', '')
+                .type('moseleyB@gmail.com')  
+                .should('have.value', 'moseleyB@gmail.com')
+                
+            passwordInput()
+                .should('have.value', '')
+                .type('password123')
+                .should('have.value', 'password123')
+
+            wrkPref()
                 .first()
-                .check(1)
+                .check()
+
+            termsInput()
+                .check()
+                .should('be.checked')
+
+            submitBtn()
+                .should('be.enabled')
+                .click()
+
+            workerElem()
+                .should('exist')
+                
         })
     })
 
