0dcb2e63d964fd54430f700f3004008e95cca77c
David Fletcher <davidfletcher@Davids-Air-2.lan>
2021-09-17 01:06:21 -0700

    cypress testing everything present in dom

--- user-onboarding/cypress/integration/form_test.spec.js
+++ user-onboarding/cypress/integration/form_test.spec.js
@@ -6,13 +6,26 @@ describe('quotes app', () =>{
 
     const firstNameInput = () => cy.get('input[name=first_name]');
     const lastNameInput = () => cy.get('input[name=last_name]');
+    const emailInput = () => cy.get('input[name=email]');
+    const passInput = () => cy.get('input[name=password]');
+    const termsCheck = () => cy.get('input[type=checkbox]')
     const submitBtn = () => cy.get('button[id=submitBtn]');
-    const textInput = () => cy.get('input[name=text]');
 
     it('sanity check', () => {
         expect(1 + 2).to.equal(3);
     })
 
+    it('proper elements showing', () =>{
+        firstNameInput().should('exist');
+        lastNameInput().should('exist');
+        emailInput().should('exist');
+        passInput().should('exist');
+        termsCheck().should('exist');
+        submitBtn().should('not.exist');
+        cy.contains('Name').should('exist');
+    })
+
+    
 
 
 })
