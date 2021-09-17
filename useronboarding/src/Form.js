9c38f3c6bab7344615e6b9a8a118ba76074f691e
Matthew Stack <matthewstack@Matthews-MacBook-Pro-4.local>
2021-09-16 20:20:25 -0400

    fixed disabled button styling

--- useronboarding/src/Form.js
+++ useronboarding/src/Form.js
@@ -99,7 +99,7 @@ export default function Form(props) {
               <button
                 id="submitBtn"
                 disabled={disabled}
-                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-6"
+                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-6 disabled:opacity-50"
               >
                 Submit
               </button>
