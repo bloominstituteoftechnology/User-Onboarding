82f98b9d1ff792cecf43b634143dc158b9105e07
LukeSchmude <schmude2002@gmail.com>
2021-10-13 17:16:02 -0500

    working on app/ postnew and validate setup

--- useronboarding/src/App.js
+++ useronboarding/src/App.js
@@ -1,7 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import OnBoardingForm from './component/Form';
+import axios from 'axios';
+import schema from './validation/formSchema';
+import * as yup from 'yup';
+import { validate } from 'uuid';
+
+
+const intialFormValues = {
+  username: '',
+  email: '',
+  password: '',
+  tos: false,
+}
+
+const intialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+}
+
+const initialForm = []
+const instialDisabled = true
 
 function App() {
+
+  const [form, setForm] = useState(initialForm)
+  const [formValues, setFormValues] = useState(intialFormValues)
+  const[formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(instialDisabled)
+
+
+  axios.get('https://reqres.in')
+    .then(res => {
+      setForm=(res.data);
+    }).catch(err => {
+      console.log(err);
+    }).finally(() => {
+      setFormValues(intialFormValues);
+    })
+
   return (
     <div className="App">
       <header className="App-header">
