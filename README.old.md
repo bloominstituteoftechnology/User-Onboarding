6ac394fee8af7005c3a41941c3ff7837b5487eb6
Christian <christiansmith2394@gmail.com>
2021-10-13 16:06:18 -0400

    Creating variables

--- README.old.md
+++ README.old.md
@@ -20,10 +20,10 @@ We've seen many different styles of form management by now -- simple to complex.
 
 We want to create a form to onboard a new user to our system. We need _at least_ the following pieces of information about our new user:
 
-- [ ] Name
-- [ ] Email
-- [ ] Password
-- [ ] Terms of Service (checkbox)
+- [x] Name
+- [x] Email
+- [x] Password
+- [x] Terms of Service (checkbox)
 - [ ] A Submit button to send our form data to the server.
 
 #### Implement Form Validation and Error Messaging
--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios';
-import Form from './Components/Form'
-import Validate from './Validator/Validate'
-import * as yup from 'yup'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Form from "./Components/Form";
+import Validate from "./Validator/Validate";
+import * as yup from "yup";
 
-function App() {
+const initialFormValues = {
+  name: "",
+  email: "",
+  password: "",
+  tos: false,
+};
+
+const initialFormErrors = {
+  name: "",
+  email: "",
+  password: "",
+  tos: "",
+};
+
+const initialEmp = [];
+
+const initialDisabled = true;
+
+export default function App() {
+
+  const [emp, setEmp] = useState(initialEmp)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const getEmp = () => {
+    axios.get('https://reqres.in/api/users')
+    .then(res => {
+      setEmp(res.data)
+    }).catch(err => {
+      console.error(err)
+    })
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
 
-export default App;
