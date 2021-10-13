db63eb4415afac1b20842f29989f8c5555bd6654
Ryan Howard <ryanghoward@protonmail.com>
2021-10-13 12:23:10 -0700

    App reset & Header in place

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './components/Form';
+import axios from 'axios';
+import * as yup from 'yup';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my App. Now give me all of your info! Muahahahaha 😈</h1>
+    </div>
+  );
+}
+
+export default App;
