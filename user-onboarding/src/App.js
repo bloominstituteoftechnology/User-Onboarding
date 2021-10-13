0aee7d23aa2f102641856643246ddc41da19bbdb
Lia Moua <moua0061@yahoo.com>
2021-10-13 15:22:26 -0400

    imported stuff and working on forms

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import Form from './Components/Form'
 import './App.css';
+import axios from 'axios'
+// import schema here
+import * as yup from 'yup'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header><h1>Hello World!</h1></header>
     </div>
-  );
+  )
 }
-
-export default App;
