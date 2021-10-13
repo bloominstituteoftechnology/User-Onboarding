dbe9feffd0378c3081556da5f8fcaa13fc479161
Vernon Bengtson <plybyvern6@aol.com>
2021-10-13 16:20:42 -0500

    form_schema set up

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import * as yup from 'yup' ;
+import schema from '../validation/form_schema';
+
+const initial_form_values = {
+  username: "",
+  email: "",
+  password: "",
+  terms_of_service: false,
+}
+
+const initial_persons = [];
+const initial_disabled = true;
+
 
 function App() {
+  const [friends, set_friends] = useState(initial_persons);
+  const [form_values, set_form_values] = useState(initial_form_values);
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
---
+++ user-onboarding/validation/form_schema.js
@@ -0,0 +1,63 @@
+import * as yup from 'yup';
+
+const form_schema = yup.object().shape({
+    username: yup
+        .string()
+        .trim()
+        .required("Username is required.")
+        .min(3, "Username must be 3 or more characters in length"),
+    email: yup
+        .string()
+        .email("Must be a valid email address")
+        .required("Email is required"),
+    password: yup
+        .string(),
+    terms_of_service: yup.boolean(),
+})
+
+
+export default form_schema;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
