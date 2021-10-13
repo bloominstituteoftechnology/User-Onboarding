7ea2ee8743877922873cf15ccbbdec54a8e22bf7
Peter Conley <peterdavidconley@gmail.com>
2021-10-13 15:30:55 -0400

    rendered user component for each initial user

--- user-onboarding/src/components/User.js
+++ user-onboarding/src/components/User.js
@@ -5,7 +5,7 @@ function User (props) {
     const { name, email, password, tos} = props
 
 return (
-    <div>
+    <div className='user'>
         <h3>Name: {/*userdetails.name*/}</h3>
         <p>Email: {/*userdetails.email*/}</p>
     </div>
--- user-onboarding/src/components/UserList.js
+++ user-onboarding/src/components/UserList.js
@@ -1,23 +1,23 @@
 import '../App.css';
-
 import User from './User'
 
 function UserList (props) {
 
     const { users } = props
+    console.log(users)
 
     return (
         <div className='User-List'>
             <h1>Users</h1>
-            { users.map( user => { return 
-            <User 
+            <div>
+            { users.map((user) => <User 
             name={user.name} 
             email={user.email} 
             password={user.password}
             tos={user.tos}
-            />}
+            />
             )
-            }
+            }</div>
         </div>
     )
 }
