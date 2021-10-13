27ed76464140b471cc7fa05fdac94d926a031561
Peter Conley <peterdavidconley@gmail.com>
2021-10-13 09:34:09 -0400

    created Form.js componenet

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form'
 
 function App() {
   return (
---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,13 @@
+function Form () {
+    return(
+        <form>
+            <label>
+                <input 
+                type='text'
+                />
+            </label>
+        </form>
+    )
+}
+
+export default Form
