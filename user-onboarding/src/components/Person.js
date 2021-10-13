a5221fe41a5ef7b6308e897af0142ff3b3158477
Vernon Bengtson <plybyvern6@aol.com>
2021-10-13 17:20:23 -0500

    making progress

--- user-onboarding/src/components/Person.js
+++ user-onboarding/src/components/Person.js
@@ -1,4 +1,18 @@
+import React from "react";
 
+function Person ({details}) {
+
+    if (!details) {
+        return <h3>Working on fetching your person's detail...</h3>
+    }
+
+    return (
+        <div className="person-container">
+            <h2>{details.username}</h2>
+            <p>Email: {details.email}</p>
+        </div>
+    )
+}
 
 
 
