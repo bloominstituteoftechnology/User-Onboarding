3941e2a216afb56a293b8b5916b60cab3ca284fc
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-10-13 19:13:10 -0700

    Form validation are ok

--- assign/src/components/Form.js
+++ assign/src/components/Form.js
@@ -9,17 +9,17 @@ export default function Form(props) {
       errors,
     } = props
 
-    const onChange = evt => {
-        const { name, value } = evt.target;
-        change(name, value);
-      }
-    
     const onSubmit = evt => {
-        evt.preventDefault();
-        submit();
-      }
-
-    
+      evt.preventDefault()
+      submit()
+    }
+  
+    const onChange = evt => {
+      /* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
+      const { name, value, checked, type } = evt.target;
+      const valueToUse = type === 'checkbox' ? checked : value;
+      change(name, valueToUse);
+    }
 
     return (
         <form className='form container' onSubmit={onSubmit}>
@@ -27,21 +27,24 @@ export default function Form(props) {
             <h2>Add a Friend</h2>   
 
             {/* 🔥 DISABLE THE BUTTON */}
-              <button >submit</button>
+            <button disabled = {disabled}>submit</button>
+            
+             <p> {console.log('disabled in Form:', disabled)} </p>
 
-            <div className=''>
+            <div className='errors'>
             {/* 🔥 RENDER THE VALIDATION ERRORS HERE */}
-              {/* <div>{errors.username}</div>
+              <div>{errors.firstname}</div>
+              <div>{errors.lastname} </div>
               <div>{errors.email}</div>
               <div>{errors.role}</div>
-              <div>{errors.civil}</div> */}
+              <div>{errors.civil}</div>
             </div>
           </div>
 
           <div className='form-group inputs'>
             <h4>General information</h4>
-            <label> Name: 
-                  <label> 
+            {/* <label> Name:  */}
+                  <label> First Name:
                     <input
                       type="text"
                       name="firstname"
@@ -51,7 +54,7 @@ export default function Form(props) {
                       placeholder="Enter your firstname here"
                     />
                   </label>
-                  <label> 
+                  <label> Last Name:
                     <input
                       type="text"
                       name="lastname"
@@ -61,7 +64,7 @@ export default function Form(props) {
                       placeholder="Enter your lasttname here"
                     />
                   </label>
-            </label>
+            {/* </label> */}
 
             <label>Password:
             <input
@@ -86,15 +89,15 @@ export default function Form(props) {
             <label>Role:          
               <select value={values.role} name="role" onChange={onChange}>
                 <option value="">-- Select a role --</option>
-                <option value="frontend">Front End</option>
-                <option value="backend">Back End</option>
-                <option value="designer">UI Designer</option>
-                <option value="teamleader">Team Leader</option>
+                <option value="Front End">Front End</option>
+                <option value="Back End">Back End</option>
+                <option value="Designer">UI Designer</option>
+                <option value="Team Leader">Team Leader</option>
               </select>
             </label>
 
-            <label>Married:
-                <label>Yes
+            {/* <label>Married: */}
+                <label>Married:
                   <input
                     type="radio"
                     name="civil"
@@ -104,7 +107,7 @@ export default function Form(props) {
                   />
                 </label>
 
-                <label>No
+                <label>Single:
                   <input
                     type="radio"
                     name="civil"
@@ -113,9 +116,40 @@ export default function Form(props) {
                     checked={values.civil === "single"}
                   />
                 </label>                
-            </label>
-            
+            {/* </label>             */}
           </div>
+
+          <div className='form-group checkboxes'>
+            <h4>TOS</h4>
+            {/* ////////// CHECKBOXES ////////// */}
+            <label>Full
+              <input
+                type="checkbox"
+                name="full"
+                onChange={onChange}
+                checked={values.full}
+              />
+            </label>
+
+            <label>Half
+              <input
+                type="checkbox"
+                name="half"
+                onChange={onChange}
+                checked={values.half}
+              />
+            </label>
+
+            <label>Contract
+              <input
+                type="checkbox"
+                name="constract"
+                onChange={onChange}
+                checked={values.constract}
+              />
+            </label>
+          </div>
+
         </form>
       )
 
