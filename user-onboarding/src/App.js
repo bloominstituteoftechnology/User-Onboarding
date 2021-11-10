0583fb4aff36e6fc9eba63d86247f0793095a76d
John Merz <jmerz826@gmail.com>
2021-11-10 15:30:52 -0500

    project set up

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
