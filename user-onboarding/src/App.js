3f5fa34c5dcd7fc8debba846fc65be63f25c85c4
SheebaHashmi <sheebahashmi0@gmail.com>
2021-11-10 13:50:27 -0500

    Create Form.js

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Form from './components/Form';
+function App() {
+  return (
+    <div className="App">
+      Hey from App
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Form(){
+    return(
+        <div>hello Form</div>
+    )
+}
