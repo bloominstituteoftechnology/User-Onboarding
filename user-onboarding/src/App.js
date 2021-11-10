2f9069112073f4a5503e2138c8c55125ab427bb3
John Merz <jmerz826@gmail.com>
2021-11-10 15:58:58 -0500

    form linked to App

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
       <Form />
     </div>
   );
---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Form = (props) => {
+    return (
+        <h1>Hiya</h1>
+    )
+}
+
+export default Form;
