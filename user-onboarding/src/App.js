d6de4122e434f5d8a34087cc918d7ffce1760d1e
Mallory Shea <malloryshea@Mallorys-MacBook-Air.local>
2021-11-11 15:10:29 -0600

    final

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import './Components.Form.js'
+import axios from 'axios';
+
+import schema from './Validation/formSchema';
+
+import * as yup from 'yup';
+
+import Form from './Components/Form';
+
+const initialFormValues = {
+  username: '',
+  password:'',
+  email:'',
+  tos: false
+}
+
+const initialFormErrors = {
+  username: '',
+  password:'',
+  email:'',
+  tos: ''
+}
 
 function App() {
-  return (
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [users, setUsers] = useState([]);
+
+  const handleSubmit = () => {
+    axios.post('https://reqres.in/api/users', formValues)
+    .then(res => {
+      setUsers([ res.data, ...users ]);
+    })
+    .catch(err => console.error())
+    .finally(() => setFormValues(initialFormValues))
+
+  }
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ... formErrors, [name]: err.errors[0] }))
+  }
+
+  const handleChange = (name, value) => {
+    validate(name, value);
+    setFormValues({ ...formValues, [name]: value });
+ }  
+ 
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form 
+        values={formValues} 
+        change={handleChange} 
+        errors={formErrors} 
+        submit= {handleSubmit} 
+      />
+      {users.map(user => (
+        <div key={user.id}>
+          <p>{user.createdAt}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
