7bfec9e6ee6eb881e9120d42a46cad7c44793c3e
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2021-11-11 02:50:53 -0500

    Eeeeh

---
+++ user-onboarding/src/validation/formSchema.js
@@ -0,0 +1,20 @@
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+    username: yup.string().trim()
+    .required('Username is required')
+    .min(3, 'Username must be 3 characters long!'),
+
+    email: yup.string()
+    .email('Must be a valid email address')
+    .required('Email is required'),
+
+    password: yup.string()
+    .required('Password is required')
+    .min(6, "Password must be 6 characters long"),
+
+    tos: yup.boolean()
+    .oneOf([true], 'Must accept the terms first.')
+})
+
+export default formSchema;
