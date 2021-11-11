f4fc94ff49984b9fc2ae0c631a560665762ba919
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2021-11-10 18:23:26 -0600

    completed schema component

--- app-name/src/validation/FormSchema.js
+++ app-name/src/validation/FormSchema.js
@@ -1,5 +1,23 @@
 import * as yup from 'yup';
 
 const formSchema = yup.object().shape({
-    
+    username: yup
+        .string()
+        .trim()
+        .required('Username is required')
+        .min(3, 'Username has to be atleast 3 characters'),
+    email: yup 
+        .string()
+        .email('Please enter a valid email')
+        .required('Email is required'),
+    password: yup
+        .string()
+        .required('A password is required')
+        .trim()
+        .min(3, 'Password must be atleast 3 characters long'),
+    terms: yup 
+        .boolean()
+        .oneOf([true], 'Must accept terms')
 })
+
+export default formSchema;
