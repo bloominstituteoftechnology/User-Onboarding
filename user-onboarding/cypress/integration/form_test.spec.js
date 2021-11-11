bc62b95b6a042b1b18305f6f5aaba9e0bbbd3759
Ryan Howard <ryanghoward@protonmail.com>
2021-11-11 13:08:24 -0800

    More sanity checks added

--- user-onboarding/cypress/integration/form_test.spec.js
+++ user-onboarding/cypress/integration/form_test.spec.js
@@ -8,6 +8,9 @@ describe("User Onboarding App", () => {
 
   it("Sanity check to make sure tests are working", () => {
     expect(2 + 2).to.equal(4);
+    expect(50 + 50).to.equal(100);
+    expect({}).not.to.equal({});
+    expect({}).to.eql({});
   });
 
   it("the proper elements are showing", () => {
