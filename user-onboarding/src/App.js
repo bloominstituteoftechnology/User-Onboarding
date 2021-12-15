9ccc2f1726be0afa13b47c4c91b057f38d147306
Kim Nguyen <kimnivore@gmail.com>
2021-12-15 11:09:14 -0800

    initial

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import axios from 'axios';
 import * as yup from 'yup';
@@ -7,18 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
