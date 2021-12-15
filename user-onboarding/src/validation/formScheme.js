f056ff3a6a54f001dcc88096578fe449795b72a3
williamvelichko <williamvelichko2003@gmail.com>
2021-12-15 13:31:06 -0800

    i did most of the work

---
+++ user-onboarding/src/validation/formScheme.js
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+const formScheme = yup.object().shape({
+  firstName: yup.string().trim().required("FirstName is required"),
+  lastName: yup.string().trim().required("LastName is required"),
+  email: yup
+    .string()
+    .email("gotta be a valid email")
+    .required("You forgot to enter email"),
+  password: yup
+    .string()
+    .required("No password provided")
+    .min(6, "password should should contain minimum 6 character"),
+  service: yup.boolean(),
+});
+
+export default formScheme;
