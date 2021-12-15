4d930b326d365b112782aee200c34fb81dbd791a
lomelo_x <aalomelo@gmail.com>
2021-12-15 14:21:41 -0600

    Completed Task 2: Create Form

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,3 +1,90 @@
 export default function Form(props) {
-    
+    const {
+        values,
+        submit,
+        change,
+        disabled,
+        errors,
+    } = props;
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
+    const onChange = evt => {
+        const { name, value, checked, type } = evt.target;
+        const valueToUse = type === 'checkbox' ? checked  : value;
+        change(name, valueToUse)
+    }
+
+    return (
+        <form className="form container" onSubmit={onChange}>
+            <div className='form-group submit'>
+                <h3>User SignUp</h3>
+                <button disabled={disabled}>submit</button>
+            
+                <div className='errors'>
+                    <div>{errors.name}</div>
+                    <div>{errors.name}</div>
+                    <div>{errors.name}</div>
+                    <div>{errors.name}</div>
+                    <div>{errors.name}</div>
+                </div>
+            </div>
+
+            <div className='form-group inputs'>
+                <h4>User Information</h4>
+                <label>First Name
+                    <input
+                        value={values.firstname}
+                        onChange={onChange}
+                        name='firstname'
+                        type='text'
+                    />
+                </label>
+                <label>Last Name
+                    <input
+                        value={values.lastname}
+                        onChange={onChange}
+                        name='lastname'
+                        type='text'
+                    />
+                </label>
+                <label>Username&nbsp;
+                    <input
+                        value={values.username}
+                        onChange={onChange}
+                        name='username'
+                        type='text'
+                    />
+                </label>
+                <label>Email
+                    <input
+                        value={values.email}
+                        onChange={onChange}
+                        name='email'
+                        type='text'
+                    />
+                </label>
+                <label>Password
+                    <input
+                        value={values.password}
+                        onChange={onChange}
+                        name='password'
+                        type='text'
+                    />
+                </label>
+                <label>Terms of Servive
+                    <input
+                        type='checkbox'
+                        name='termsofservice'
+                        checked={values.termsofservice}
+                        onChange={onChange}
+                    />
+                </label>
+            </div> 
+        </form>
+    )
 }
+
