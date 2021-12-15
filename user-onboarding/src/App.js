83c2ebedc6948da977fe76cca314db10e0f8a25f
Chris Kelley <chriskelley85@gmail.com>
2021-12-15 12:51:52 -0600

    form renders with name, email, password, tos, button

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './Components/Form';
+
+const initialFormValues = {
+  username: '',
+  password: '',
+  email: '',
+  checked: false
+}
 
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  
+  const handleSubmit = () => {
+    
+  }
+
+  const handleChange = (name, value) => {
+    setFormValues({...formValues, [name]: value});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form values={formValues} change={handleChange} />
     </div>
   );
 }
---
+++ user-onboarding/src/Components/Form.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+
+const Form = (props) => {
+    const { change, submit } = props;
+    const { username, email, password, checked } = props.values;
+
+    const onChange = (e) => {
+        const {name, value, checked, type }= e.target;
+        const newVal = type === 'checkbox' ? checked : value;
+        change(name, newVal);
+    }
+
+    const onsubmit = (e) => {
+        e.preventDefault();
+        submit();
+    }
+    return (
+        <div>
+        <h1>My Radtastic Form</h1>
+        <form>
+            <label>Name:
+                <input
+                type="text"
+                name="user"
+                value={username}
+                onChange={onChange}
+            />
+            </label>
+            <label>Email:
+                <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={onChange}
+                />
+            </label>
+            <label>Password 
+                <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={onChange}
+                />
+            </label>
+            <label>Terms of Service:
+                <input 
+                type="checkbox"
+                name="tos"
+                checked={checked}
+                onChange={onChange}
+                />
+            </label>
+            <input type="submit" value="Create a Friend"/>
+        </form>
+        </div>
+    )
+}
+
+export default Form;
