32461836ffd70e492bb0e811d7cef67736ed931a
Shamir Gibbons <nonshalant@shamirs-air.myfiosgateway.com>
2021-12-15 21:02:02 -0500

    update on the design

--- classwork/src/App.js
+++ classwork/src/App.js
@@ -5,32 +5,84 @@ import styled from 'styled-components';
 
 
 const StyledForm = styled.div`
-border: 1px solid black;
-height: 40vh;
-width: 75%;
+border: 1px solid whitesmoke;
+background-color: #4CB1B3;
+box-shadow: whitesmoke 4px 20px 20px;
+height: 80vh;
+width: 85%;
 margin: auto;
-margin-top: 20%;
+margin-top: 9%;
+color: whitesmoke;
+
+
+.title{
+  color: whitesmoke;
+  text-align: center;
+}
+
+.content{
+  display: flex;
+  justify-content: space-evenly;
+  height: 50vh;
+  text-align: center;
+}
+
+.divOne {
+  display:flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  
+}
+
+.divThree {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+.musicChoice{
+  display:
+}
 `
 
 function App() {
   return (
     <StyledForm>
-      <h1 className='title'> Sign Up for The Hoo App </h1>
-      <h3 className='generalInfo'>General Information</h3>
+      <h1 className='title'> Welcome To Hoo! </h1>
+      <h3 className='musicChoice'>General Information</h3>
       <h3 className='musicChoice'>Pick a Genre of music</h3>
+
       <form>
-        <input type='text' name='username'/>
-        <input type='email' name='email'/>
-        <select>
-          <option>-- Please Select A Gender --</option>
-          <option>-- Male --</option>
-          <option>-- Female --</option>
-        </select>
-        <input />
-        <input/>
-        <input/>
-        <input/>
-        <input/>
+        <div className='content'>
+
+        <div className='divOne'>
+          <label>Username
+            <input type='text' name='username'/>
+          </label>
+          <label>Add Email
+            <input type='email' name='email'/>
+          </label>
+          <label>Password
+            <input type='password' name='password'/>
+          </label>
+          <select>
+            <option>-- Please Select A Gender --</option>
+            <option>-- Male --</option>
+            <option>-- Female --</option>
+          </select>
+        </div>
+       
+       <div className='divThree'>
+        <label>Over 18
+            <input type='radio' name='age'/>
+          </label>
+          <label>Under 18
+            <input type='radio' name='age'/>
+          </label>
+          <label>Number
+            <input type='number' name='number'/>
+          </label>
+       </div>
+        </div>
       </form>
     </StyledForm>
   );
