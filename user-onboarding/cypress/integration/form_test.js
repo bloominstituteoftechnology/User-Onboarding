19f840b1b3910d9e1afb1675bbb7e8d83eec414d
David Conley <dconley1989@gmail.com>
2021-12-16 15:29:19 -0700

    added test for email and password typing into input and a test to check if you can check the checkbox

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -6,7 +6,7 @@ describe("User Onboarding App", () => {
   const firstNameInput = () => cy.get("input[name=first_name]");
   const emailInput = () => cy.get("input[name=email]");
   const passwordInput = () => cy.get("input[name=password]");
-  const tosCheckbox = () => cy.get("input[name=termsOfService]");
+  const tosCheckbox = () => cy.get("input[type='checkbox']");
   const submitButton = () => cy.get("button");
 
   describe("be able to type a name into the inputs", () => {
@@ -14,11 +14,23 @@ describe("User Onboarding App", () => {
       cy.url().should("include", "localhost:3000");
     });
 
-    it("can type into input", () => {
+    it("can type into input name and check terms of service", () => {
       firstNameInput()
         .should("have.value", "")
         .type("Jerry")
         .should("have.value", "Jerry");
+    });
+
+    it("can type into input email", () => {
+      emailInput().should("have.value", "").type("jerry@gmail.com");
+    });
+
+    it("can type into input password", () => {
+      passwordInput().should("have.value", "").type("Seinfeld");
+    });
+
+    it("can check terms of service", () => {
+      tosCheckbox().check();
     });
   });
 });
