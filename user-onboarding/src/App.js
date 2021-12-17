de0e56958e0fdfe99b2bcc6941c83daadb60f9e8
Mark <markgabon@hotmail.com>
2021-12-16 19:20:21 -0500

    Added onboarding

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,97 +2,71 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React, {useState, useEffect} from 'react'
+import OnboardForm from './OnboardForm';
+
 const initialState = {
-  username: '',
+  name: '',
   email: '',
-  skills: '',
-  civil: ''
+  password: '',
+  terms: false
 }
 
 const initialFriends = [];
+
 function App() {
 
 const [friends, setFriends] = useState(initialFriends)
 const [formValues, setFormValues] = useState(initialState)
 
-const change = (evt) => {
-  const { value, name } = evt.target
-  setFormValues({...formValues, [name]: value})
+const getFriends = () => {
+  axios.get('https://reqres.in/api/users')
+  .then(resp => {
+    setFriends(resp.data);
+  }).catch(err => console.error(err))
+}
+
+const postNewFriend = newFriend => {
+  axios.post('https://reqres.in/api/users', newFriend)
+  .then(resp => {
+    setFriends([resp.data, ...friends])
+  }).catch(err => console.error(err))
+  .finally(() => setFormValues(initialState))
+}
+
+
+const change = (name, value) => {
+  validate(name, value);
+  setFormValues({
+    ...formValues,
+    [name]: value
+  })
 }
 
 
 const submit = (evt) => {
   evt.preventDefault();
   const newFriend = {
-    username: formValues.username.trim(),
+    name: formValues.name.trim(),
     email: formValues.email.trim(),
-    skills: formValues.skills.trim(),
-    civil: formValues.civil.trim(),
+    password: formValues.password.trim(),
   }
 
-setFriends(friends.concat(newFriend));
-setFormValues(initialState)
+  postNewFriend(newFriend)
 }
+
+useEffect(() => {
+  getFriends()
+}, [])
 
   return (
     <div className="App">
       <header className="App-header">
-      <div className = "friends">
-        {friends.map((person, idx) =>(
-          <div key={idx}>
-          {person.username.toUpperCase()} certifications: {person.skills.toUpperCase()} contact: {person.email}
-
-          </div>
-        ))}
-        <form onSubmit={submit}>
-        <label className='username'>Username: </label>
-        <input 
-        value={formValues.username}
-        onChange={change}
-        name="username"
-        type="text"
-        placeholder="enter desired username"
-        />
-        <label className='email'>Email: </label>
-        <input 
-        value={formValues.email}
-        onChange={change}
-        name="email"
-        type='text'
-        placeholder='enter email'
-        />
-        <label className='skills'>Skills: </label>
-        <input 
-        value={formValues.skills}
-        onChange={change}
-        name='skills'
-        type='text'
-        placeholder='enter your skills here'
-        />
-        <button>Create Associate</button>
-        <div className='civil'>
-          Marital Status
-        </div>
-        <label>
-          Single
-          <input 
-          type='radio'
-          name='civil'
-          value='single'
-          onChange={change}
-          />
-        </label>
-        <label>
-          Married
-          <input 
-          type='radio'
-          name='civil'
-          value='married'
-          onChange={change}
-          />
-        </label>
-        </form>
-      </div>
+      <h1>Onboarding Form</h1>
+      <OnboardForm 
+      values={formValues}
+      change={change}
+      submit={submit}
+      />
       </header>
     </div>
   );
