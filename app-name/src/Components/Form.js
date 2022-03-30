29be97361744e9f7b265a9e765e394bfe1d16041
Bennett Kautz <bennettkautz@gmail.com>
2022-03-30 18:25:34 -0400

    done

--- app-name/src/Components/Form.js
+++ app-name/src/Components/Form.js
@@ -1,12 +1,28 @@
 import React from'react'
 
 const Form = (props) => {
-    const { onChange }= props;
+    const { onChange, onSubmit }= props;
     const { username, email, password, tos } = props.values;
+
+    onChange= (e) => {
+        const { name, value, checked, type} = e.target;
+        const newVal = type === 'checkbox' ? checked : value;
+        change(name, newVal);
+    } 
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        submit();
+    }
 
     return (
         <div>
             <h1>Yuh</h1>
+            <p>{errors.username}</p>
+            <p>{errors.password}</p>
+            <p>{errors.email}</p>
+            <p>{errors.tos}</p>
+            <form onSubmit={onSubmit}></form>
             <form>
                 <label>Name:
                     <input 
