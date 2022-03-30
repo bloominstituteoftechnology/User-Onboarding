0fc6100ee6b468f062252731be4282910c3b0271
Bennett Kautz <bennettkautz@gmail.com>
2022-03-30 17:42:44 -0400

    making functions

--- app-name/src/App.js
+++ app-name/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './Components/Form';
 
+
+const initialFormValues = {
+  username: '',
+  password: '',
+  email:'',
+  checked: false
+}
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+const handleSubmit = () => {
+  
+}
+
+  const handleChange = (name, value) => {
+    setFormValues({...formValues, [name]: formValues})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form values={formValues}/>
     </div>
   );
 }
