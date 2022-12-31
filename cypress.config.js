f135b7ca13b9b407f51430814b019c5abd81a030
anaiz.alfonso <anaiz.alfonso@hotmai.com>
2022-12-30 23:20:31 -0500

    completed module 4

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,11 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
 });
