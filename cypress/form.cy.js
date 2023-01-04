7e150e16b2cc559f67b832a6dea0e8f87e9d5074
anaiz.alfonso <anaiz.alfonso@hotmai.com>
2023-01-03 20:13:17 -0500

    MODULE 4 COMPLETE

--- cypress/form.cy.js
+++ cypress/form.cy.js
@@ -13,17 +13,7 @@ describe('Quotes App', () => {
     const passwordInput = () => cy.get('input[name=password]')
     const tosCkbx = () => cy.get(`radio[id="toscheckbx"]`)
     const cancelBtn = () => cy.get(`button[id="cancelBtn"]`)
-  
-    it('sanity check to make sure tests work', () => {
-      // "it" is a test.
-      // "expect" is an assertion.
-      // There can be several assertions per test, but they all need to relate
-      // to "the one thing" we're testing.
-      expect(1 + 2).to.equal(3)
-      expect(2 + 2).not.to.equal(5) // strict ===
-      expect({}).not.to.equal({})   // strict ===
-      expect({}).to.eql({})         // not strict
-    })
+})
   
     it('the proper elements are showing', () => {
       textInput().should('exist')
